13b8a1dd3e4811774afe76961995541860dcdf15
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-05-28 13:48:20 -0300

    :zap: update

--- src/shared/system/constant/application-variable/utils/environment-variable.object.ts
+++ src/shared/system/constant/application-variable/utils/environment-variable.object.ts
@@ -1,6 +1,6 @@
+import 'dotenv/config';
 import { InvalidApplicationVariableTypeError } from '@shared/system/constant/application-variable/utils/error/invalid-application-variable.error';
 import { MissingApplicationVariableError } from '@shared/system/constant/application-variable/utils/error/missing-application-variable.error';
-import 'dotenv/config';
 
 export class EnvironmentVariable {
   protected readonly _type = EnvironmentVariable.name;
@@ -23,6 +23,7 @@ export class EnvironmentVariable {
       throw new InvalidApplicationVariableTypeError({
         variableName: key,
         expectedType: type.name,
+        currentValue: value,
       });
     }
 
@@ -42,21 +43,18 @@ export class EnvironmentVariable {
       return defaultValue;
     }
 
-    try {
-      const convertedValue = this.convertValue<T>(value, type);
-      const conversionFailed = convertedValue === null;
+    const convertedValue = this.convertValue<T>(value, type);
+    const conversionFailed = convertedValue === null;
 
-      if (conversionFailed) {
-        throw new InvalidApplicationVariableTypeError({
-          variableName: key,
-          expectedType: type.name,
-        });
-      }
-
-      return convertedValue;
-    } catch {
-      return defaultValue;
+    if (conversionFailed) {
+      throw new InvalidApplicationVariableTypeError({
+        variableName: key,
+        expectedType: type.name,
+        currentValue: value,
+      });
     }
+
+    return convertedValue;
   }
 
   private convertValue<T>(
--- src/shared/system/constant/application-variable/utils/error/invalid-application-variable.error.ts
+++ src/shared/system/constant/application-variable/utils/error/invalid-application-variable.error.ts
@@ -3,9 +3,13 @@ import { UnexpectedError } from '@core/error/unexpected.error';
 export class InvalidApplicationVariableTypeError extends UnexpectedError {
   protected override readonly _type = InvalidApplicationVariableTypeError.name;
 
-  public constructor(props: { variableName: string; expectedType: string }) {
+  public constructor(props: {
+    variableName: string;
+    expectedType: string;
+    currentValue: string;
+  }) {
     super(
-      `A variável ${props.variableName} não é um tipo válido para ${props.expectedType}`,
+      `A variável "${props.variableName}" deveria ser do tipo "${props.expectedType}", mas o valor recebido foi "${props.currentValue}", que não é compatível.`,
     );
   }
 }
