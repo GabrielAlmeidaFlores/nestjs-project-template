cb2ab07643f259abbffb1175b31559b2cbc19986
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-06-16 18:08:43 -0300

    Fix: formattation

--- src/core/domain/schema/entity/bank/bank-payment/bank-payment.entity.props.interface.ts
+++ src/core/domain/schema/entity/bank/bank-payment/bank-payment.entity.props.interface.ts
@@ -2,16 +2,17 @@ import type { PaymentMethodEnum } from '@core/domain/schema/entity/bank/bank-pay
 import type { StatusEnum } from '@core/domain/schema/entity/bank/bank-payment/enum/payment-status.enum';
 import type { BaseEntityPropsInterface } from '@core/domain/schema/entity/base/base.entity.props.interface';
 
-export interface BankPaymentEntityPropsInterface extends BaseEntityPropsInterface {
-    bankExternalId: string;
-    paymentMethod: PaymentMethodEnum;
-    value: string;
-    netValue: string;
-    discountPercentage: number;
-    status: StatusEnum;
-    dueDate: Date;
-    paymentDate: Date | null;
-    installmentNumber: number | null;
-    costumerConfirmDate: Date | null;
-    pixQrCode: string | null;
+export interface BankPaymentEntityPropsInterface
+  extends BaseEntityPropsInterface {
+  bankExternalId: string;
+  paymentMethod: PaymentMethodEnum;
+  value: string;
+  netValue: string;
+  discountPercentage: number;
+  status: StatusEnum;
+  dueDate: Date;
+  paymentDate: Date | null;
+  installmentNumber: number | null;
+  costumerConfirmDate: Date | null;
+  pixQrCode: string | null;
 }
