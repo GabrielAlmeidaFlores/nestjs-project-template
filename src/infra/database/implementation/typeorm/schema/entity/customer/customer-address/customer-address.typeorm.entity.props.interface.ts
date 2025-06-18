ce6a473c1413e0f80a0fad2013d2844defccf04d
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-06-18 16:07:14 -0300

    :sparkles: feature: customer-address

---
+++ src/infra/database/implementation/typeorm/schema/entity/customer/customer-address/customer-address.typeorm.entity.props.interface.ts
@@ -0,0 +1,13 @@
+import type { CustomerEntity } from '@core/domain/schema/entity/customer/customer/customer.entity';
+import type { StateCodeEnum } from '@core/domain/schema/enum/state-code.enum';
+import type { BaseTypeormEntityPropsInterface } from '@infra/database/implementation/typeorm/schema/entity/base/base/base.typeorm.entity.props.interface';
+
+export interface CostumerAddressTypeormEntityPropsInterface
+  extends BaseTypeormEntityPropsInterface {
+  postalCode: string;
+  stateCode: StateCodeEnum;
+  city: string;
+  neighborhood: string;
+  addressNumber: string;
+  customer: CustomerEntity;
+}
