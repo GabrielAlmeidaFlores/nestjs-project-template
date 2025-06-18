c085df68a427b4b93197b704381e404cd7adf512
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-06-18 12:27:28 -0300

    Fix: customer-adress-entity error text

--- src/core/domain/schema/entity/customer/customer-address/error/invalid-customer-adress-city.error.ts
+++ src/core/domain/schema/entity/customer/customer-address/error/invalid-customer-adress-city.error.ts
@@ -5,7 +5,7 @@ export class InvalidCityError extends InvalidInputError {
 
   public constructor(props: { maxLength: number; minLength: number }) {
     super(
-      `O nome da cidade deve conter de ${props.minLength} a ${props.maxLength} caracteres e pode incluir apenas letras, espaços e caracteres especiais (acentuação).`,
+      `O nome da cidade deve conter de ${props.minLength} a ${props.maxLength} caracteres e pode incluir apenas letras, espaços e caracteres especiais.`,
     );
   }
 }
