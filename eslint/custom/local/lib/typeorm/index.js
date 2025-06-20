e7fefb957f6ad529583815665f37749f90c1787b
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-06-20 14:16:07 -0300

    feat: sparkles: require-bidirectional-typeorm-relation ESLint rule

--- eslint/custom/local/lib/typeorm/index.js
+++ eslint/custom/local/lib/typeorm/index.js
@@ -4,12 +4,14 @@ module.exports = {
         rules: {
           'require-nullable-type-on-nullable-column': require('./require-nullable-type-on-nullable-column.js'),
           'restrict-column-type': require('./restrict-column-type.js'),
+          'require-bidirectional-typeorm-relation': require('./require-bidirectional-typeorm-relation.js'),
         },
       },
     },
     rules: {
       'typeorm-rule/require-nullable-type-on-nullable-column': 'error',
       'typeorm-rule/restrict-column-type': 'error',
+      'typeorm-rule/require-bidirectional-typeorm-relation': 'error',
     },
   };
   
