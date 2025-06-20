896b2180df59e6e3c50703698b1d8142308614e1
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-06-20 16:00:24 -0300

    :hammer: These changes aim to streamline entity interactions and simplify future modifications.

--- src/infra/database/implementation/typeorm/schema/entity/available-payment-plan/available-payment-plan/available-payment-plan.typeorm.entity.ts
+++ src/infra/database/implementation/typeorm/schema/entity/available-payment-plan/available-payment-plan/available-payment-plan.typeorm.entity.ts
@@ -1,7 +1,6 @@
 import { Column, Entity, OneToMany } from 'typeorm';
 
 import { PaymentPlanCycleEnum } from '@core/domain/schema/enum/payment-plan/payment-plan-cycle.enum';
-import { DecimalValue } from '@core/domain/schema/value-object/decimal/decimal.value-object';
 import { AvailablePaymentPlanTypeormEntityPropsInterface } from '@infra/database/implementation/typeorm/schema/entity/available-payment-plan/available-payment-plan/available-payment-plan.typeorm.entity.props.interface';
 import { AvailablePaymentPlanEnabledPaidResourceTypeormEntity } from '@infra/database/implementation/typeorm/schema/entity/available-payment-plan/available-payment-plan-enabled-paid-resource/available-payment-plan-enabled-paid-resource.typeorm.entity';
 import { BaseTypeormEntity } from '@infra/database/implementation/typeorm/schema/entity/base/base/base.typeorm.entity';
@@ -11,29 +10,29 @@ export class AvailablePaymentPlanTypeormEntity extends BaseTypeormEntity {
   @Column({ name: 'name', type: 'varchar', length: 50 })
   public name: string;
 
-  @Column({ name: 'description', type: 'varchar', length: 50 })
+  @Column({ name: 'description', type: 'varchar', length: 255 })
   public description: string;
 
-  @Column({ name: 'price', type: 'decimal', length: 50 })
-  public price: DecimalValue;
+  @Column({ name: 'price', type: 'decimal' })
+  public price: string;
 
-  @Column({ name: 'max_member_limit', type: 'number', length: 50 })
+  @Column({ name: 'max_member_limit', type: 'number' })
   public maxMemberLimit: number;
 
-  @Column({ name: 'monthly_credit_amount', type: 'number', length: 50 })
+  @Column({ name: 'monthly_credit_amount', type: 'number' })
   public monthlyCreditAmount: number;
 
-  @Column({ name: 'active', type: 'bool', length: 50 })
+  @Column({ name: 'active', type: 'boolean' })
   public active: boolean;
 
-  @Column({ name: 'cycle', type: 'enum', length: 50 })
+  @Column({ name: 'cycle', type: 'simple-enum' })
   public cycle: PaymentPlanCycleEnum;
 
   @OneToMany(
     () => AvailablePaymentPlanEnabledPaidResourceTypeormEntity,
     (entity) => entity.availablePaymentPlan,
   )
-  public availablePaymentPlan:
+  public availablePaymentPlanEnabledPaidResource:
     | AvailablePaymentPlanEnabledPaidResourceTypeormEntity[]
     | undefined;
 
@@ -54,6 +53,7 @@ export class AvailablePaymentPlanTypeormEntity extends BaseTypeormEntity {
     this.monthlyCreditAmount = props.monthlyCreditAmount;
     this.active = props.active;
     this.cycle = props.cycle;
-    this.availablePaymentPlan = props.availablePaymentPlan;
+    this.availablePaymentPlanEnabledPaidResource =
+      props.availablePaymentPlanEnabledPaidResource;
   }
 }
