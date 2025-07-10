da610a2ecdaac189deccd1de8d36965491f6ed1a
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-07-10 09:54:42 -0300

    Refactor: update BankTransfer entity and related mappings to use TransferStatusEnum; add TransferStatusEnum definition

--- src/core/domain/schema/entity/bank/bank-transfer/bank-transfer.entity.props.interface.ts
+++ src/core/domain/schema/entity/bank/bank-transfer/bank-transfer.entity.props.interface.ts
@@ -1,8 +1,8 @@
 import type { BankPaymentEntity } from '@core/domain/schema/entity/bank/bank-payment/bank-payment.entity';
 import type { TransferMethodEnum } from '@core/domain/schema/entity/bank/bank-transfer/enum/transfer-method.enum';
+import type { TransferStatusEnum } from '@core/domain/schema/entity/bank/bank-transfer/enum/transfer-status.enum';
 import type { BaseEntityPropsInterface } from '@core/domain/schema/entity/base/base/base.entity.props.interface';
 import type { DecimalValue } from '@core/domain/schema/value-object/decimal/decimal.value-object';
-import type { Status } from '@core/domain/schema/value-object/status/status.value-object';
 
 export interface BankTransferEntityPropsInterface
   extends BaseEntityPropsInterface {
@@ -11,7 +11,7 @@ export interface BankTransferEntityPropsInterface
   transferMethod: TransferMethodEnum;
   value: DecimalValue;
   netValue: DecimalValue;
-  status: Status;
+  status: TransferStatusEnum;
   effectiveDate?: Date | null;
   scheduleDate?: Date | null;
   failReason?: string | null;
