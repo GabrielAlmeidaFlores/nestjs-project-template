0c8200f03da1a95646b19d5404ea3449d1df513b
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-07-10 09:41:27 -0300

    Refactor: update BankTransfer entity to use TransferMethodEnum instead of TransferMethod; adjust related imports accordingly

--- src/core/domain/schema/entity/bank/bank-transfer/bank-transfer.entity.props.interface.ts
+++ src/core/domain/schema/entity/bank/bank-transfer/bank-transfer.entity.props.interface.ts
@@ -1,14 +1,14 @@
 import type { BankPaymentEntity } from '@core/domain/schema/entity/bank/bank-payment/bank-payment.entity';
+import type { TransferMethodEnum } from '@core/domain/schema/entity/bank/bank-transfer/enum/transfer-method.enum';
 import type { BaseEntityPropsInterface } from '@core/domain/schema/entity/base/base/base.entity.props.interface';
 import type { DecimalValue } from '@core/domain/schema/value-object/decimal/decimal.value-object';
 import type { Status } from '@core/domain/schema/value-object/status/status.value-object';
-import type { TransferMethod } from '@core/domain/schema/value-object/transfer-method/transfer-method.value-object';
 
 export interface BankTransferEntityPropsInterface
   extends BaseEntityPropsInterface {
   bankExternalId: string;
   description: string;
-  transferMethod: TransferMethod;
+  transferMethod: TransferMethodEnum;
   value: DecimalValue;
   netValue: DecimalValue;
   status: Status;
