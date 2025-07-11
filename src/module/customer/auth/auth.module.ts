605778a191d43d6eb0b0a63cddbd78a7c645cfb6
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-07-11 07:06:29 -0300

    Add: implement logout functionality and update authentication configuration

--- src/module/customer/auth/auth.module.ts
+++ src/module/customer/auth/auth.module.ts
@@ -5,12 +5,13 @@ import { DatabaseModule } from '@infra/database/database.module';
 import { UserSessionModule } from '@lib/user-session/user-session.module';
 import { AuthController } from '@module/customer/auth/auth.controller';
 import { LoginUseCase } from '@module/customer/auth/use-case/login.use-case';
+import { LogoutUseCase } from '@module/customer/auth/use-case/logout.use-case';
 import { SignUpUseCase } from '@module/customer/auth/use-case/sign-up.use-case';
 
 @Module({
   imports: [DatabaseModule, BankModule, UserSessionModule],
   controllers: [AuthController],
-  providers: [SignUpUseCase, LoginUseCase],
+  providers: [SignUpUseCase, LoginUseCase, LogoutUseCase],
 })
 export class AuthModule {
   protected readonly _type = AuthModule.name;
