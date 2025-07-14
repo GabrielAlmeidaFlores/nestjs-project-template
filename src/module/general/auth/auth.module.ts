95afaa472b137b2a38519269bc69b47e54cc8779
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-07-14 11:48:12 -0300

    refactor: remove duplicate import of CustomerSignUpUseCase in auth.module.ts

--- src/module/general/auth/auth.module.ts
+++ src/module/general/auth/auth.module.ts
@@ -4,9 +4,9 @@ import { BankModule } from '@infra/bank/bank.module';
 import { DatabaseModule } from '@infra/database/database.module';
 import { UserSessionModule } from '@lib/user-session/user-session.module';
 import { AuthController } from '@module/general/auth/auth.controller';
+import { CustomerSignUpUseCase } from '@module/general/auth/use-case/customer-sign-up.use-case';
 import { LoginUseCase } from '@module/general/auth/use-case/login.use-case';
 import { LogoutUseCase } from '@module/general/auth/use-case/logout.use-case';
-import { CustomerSignUpUseCase } from '@module/general/auth/use-case/customer-sign-up.use-case';
 
 @Module({
   imports: [DatabaseModule, BankModule, UserSessionModule],
