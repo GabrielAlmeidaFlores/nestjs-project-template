aa701791dadfc8275c3e71963e74199169cb0ed1
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-07-14 08:03:39 -0300

    refactor: update import for BaseAuditableEntity to use props interface in BankPayment entity

--- src/core/domain/schema/entity/bank/bank-payment/bank-payment.entity.props.interface.ts
+++ src/core/domain/schema/entity/bank/bank-payment/bank-payment.entity.props.interface.ts
@@ -1,6 +1,6 @@
 import type { PaymentMethodEnum } from '@core/domain/schema/entity/bank/bank-payment/enum/payment-method.enum';
 import type { PaymentStatusEnum } from '@core/domain/schema/entity/bank/bank-payment/enum/payment-status.enum';
-import type { BaseAuditableEntity } from '@core/domain/schema/entity/base/base-auditable/base-auditable.entity';
+import type { BaseAuditableEntityPropsInterface } from '@core/domain/schema/entity/base/base-auditable/base-auditable.entity.props.interface';
 import type { CustomerEntity } from '@core/domain/schema/entity/customer/customer/customer.entity';
 import type { DecimalValue } from '@core/domain/schema/value-object/decimal/decimal.value-object';
 
