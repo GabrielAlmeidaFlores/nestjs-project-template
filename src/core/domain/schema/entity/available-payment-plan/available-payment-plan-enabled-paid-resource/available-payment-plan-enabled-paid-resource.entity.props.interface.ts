21cf4e46f9e443be92c299b198806e849ff8422e
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-07-15 06:41:34 -0300

    refactor: extend AvailablePaymentPlanEnabledPaidResourceEntity with BaseEntity and update interface

--- src/core/domain/schema/entity/available-payment-plan/available-payment-plan-enabled-paid-resource/available-payment-plan-enabled-paid-resource.entity.props.interface.ts
+++ src/core/domain/schema/entity/available-payment-plan/available-payment-plan-enabled-paid-resource/available-payment-plan-enabled-paid-resource.entity.props.interface.ts
@@ -1,8 +1,10 @@
 import type { ApplicationPaidResourceEntity } from '@core/domain/schema/entity/application-resource/application-paid-resource/application-paid-resource.entity';
 import type { AvailablePaymentPlanEntity } from '@core/domain/schema/entity/available-payment-plan/available-payment-plan/available-payment-plan.entity';
+import type { BaseEntityPropsInterface } from '@core/domain/schema/entity/base/base/base.entity.props.interface';
 import type { RelationModel } from '@core/domain/schema/model/relation.model';
 
-export interface AvailablePaymentPlanEnabledPaidResourceEntityPropsInterface {
+export interface AvailablePaymentPlanEnabledPaidResourceEntityPropsInterface
+  extends BaseEntityPropsInterface {
   applicationPaidResource: RelationModel<ApplicationPaidResourceEntity>;
   availablePaymentPlan: AvailablePaymentPlanEntity;
 }
