c33b522b829a33acbbdd1cab3e6f33002a6a22e3
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-09-08 17:10:57 -0300

    feat: add auto-mapper profiles for customer, organization, and organization member entities

--- src/lib/mapper/implementation/auto-mapper/auto-mapper.module.ts
+++ src/lib/mapper/implementation/auto-mapper/auto-mapper.module.ts
@@ -3,6 +3,14 @@ import { AutomapperModule } from '@automapper/nestjs';
 import { Module } from '@nestjs/common';
 
 import { AutoMapperService } from '@lib/mapper/implementation/auto-mapper/auto-mapper.service';
+import { CustomerEntityAutoMapperProfile } from '@lib/mapper/implementation/auto-mapper/profile/database/typeorm/customer/customer-entity.auto-mapper.profile';
+import { GetCustomerQueryResultAutoMapperProfile } from '@lib/mapper/implementation/auto-mapper/profile/database/typeorm/customer/get-customer-query-result.auto-mapper.profile';
+import { CustomerAddressEntityAutoMapperProfile } from '@lib/mapper/implementation/auto-mapper/profile/database/typeorm/customer-address/customer-address-entity.auto-mapper.profile';
+import { GetCustomerAddressQueryResultAutoMapperProfile } from '@lib/mapper/implementation/auto-mapper/profile/database/typeorm/customer-address/get-customer-query-result.auto-mapper.profile';
+import { GetOrganizationQueryResultAutoMapperProfile } from '@lib/mapper/implementation/auto-mapper/profile/database/typeorm/organization/get-organization-query-result.auto-mapper.profile';
+import { OrganizationEntityAutoMapperProfile } from '@lib/mapper/implementation/auto-mapper/profile/database/typeorm/organization/organization-entity.auto-mapper.profile';
+import { GetOrganizationMemberQueryResultAutoMapperProfile } from '@lib/mapper/implementation/auto-mapper/profile/database/typeorm/organization-member/get-organization-member-query-result.auto-mapper.profile';
+import { OrganizationMemberEntityAutoMapperProfile } from '@lib/mapper/implementation/auto-mapper/profile/database/typeorm/organization-member/organization-member-entity.auto-mapper.profile';
 
 @Module({
   imports: [
@@ -10,7 +18,17 @@ import { AutoMapperService } from '@lib/mapper/implementation/auto-mapper/auto-m
       strategyInitializer: classes(),
     }),
   ],
-  providers: [AutoMapperService],
+  providers: [
+    AutoMapperService,
+    CustomerEntityAutoMapperProfile,
+    GetCustomerQueryResultAutoMapperProfile,
+    CustomerAddressEntityAutoMapperProfile,
+    GetCustomerAddressQueryResultAutoMapperProfile,
+    GetOrganizationQueryResultAutoMapperProfile,
+    OrganizationEntityAutoMapperProfile,
+    GetOrganizationMemberQueryResultAutoMapperProfile,
+    OrganizationMemberEntityAutoMapperProfile,
+  ],
   exports: [AutoMapperService],
 })
 export class AutoMapperModule {
