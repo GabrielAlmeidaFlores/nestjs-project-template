a098b15bce4496e80b5829e313c4cb8e4c0e58d7
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-09-09 09:25:56 -0300

    fix: update CustomerSignUpResponseDto to use CustomerId value object instead of Guid

--- src/module/customer/account/dto/response/customer-sign-up.response.dto.ts
+++ src/module/customer/account/dto/response/customer-sign-up.response.dto.ts
@@ -1,12 +1,12 @@
-import { Guid } from '@core/domain/schema/value-object/guid/guid.value-object';
+import { CustomerId } from '@module/customer/account/domain/schema/entity/customer/value-object/customer-id.value-object';
 import { ResponseDto } from '@shared/api/util/decorator/class/dto-specification/response-dto.decorator';
 import { ResponseDtoValueObjectProperty } from '@shared/api/util/decorator/property/dto-property/response/response-dto-value-object-property.decorator';
 import { BaseBuildableObject } from '@shared/system/object/base-buildable.object';
 
 @ResponseDto()
 export class CustomerSignUpResponseDto extends BaseBuildableObject {
-  @ResponseDtoValueObjectProperty(Guid)
-  public id: Guid;
+  @ResponseDtoValueObjectProperty(CustomerId)
+  public id: CustomerId;
 
   protected override readonly _type = CustomerSignUpResponseDto.name;
 }
