ae32644de397e0a0f3e642022209e50792575549
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-09-10 13:26:17 -0300

    feat: implement AuthIdentity module with session management and local cache storage

--- src/lib/authenticator/authenticator.gateway.ts
+++
@@ -1,9 +0,0 @@
-import type { AuthenticatorCredentialsOutputModel } from '@lib/authenticator/implementation/speakeasy/output/authenticator-credentials.output.model';
-
-export abstract class AuthenticatorGateway {
-  public abstract verifyCode(secret: string, token: string): boolean;
-
-  public abstract generateCredentials(
-    username: string,
-  ): Promise<AuthenticatorCredentialsOutputModel>;
-}
