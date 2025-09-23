e1d8b72e6df5d4b3b70b022d2b7d7948680c1b18
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-09-23 07:22:19 -0300

    :zap: add: file-processor service

---
+++ src/module/customer/account/lib/file-processor/file-processor.gateway.ts
@@ -0,0 +1,10 @@
+export abstract class FileProcessorGateway {
+  public abstract processAndUploadProfilePicture(
+    profilePicture: Buffer,
+    profilePictureLocation?: string,
+  ): Promise<string>;
+
+  public abstract getProfilePicture(
+    profilePictureLocation: string,
+  ): Promise<URL>;
+}
