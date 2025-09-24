ec15a14ef0334e7aaab9b72983b25915d156ea98
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-09-24 14:35:19 -0300

    :zap: update: GetOrganizationMemberQueryResult

--- src/module/customer/account/domain/repository/organization-member/query/result/get-organization-member.query.result.ts
+++ src/module/customer/account/domain/repository/organization-member/query/result/get-organization-member.query.result.ts
@@ -1,13 +1,9 @@
 import { BaseBuildableObject } from '@shared/system/object/base-buildable.object';
 
-import type { GetCustomerQueryResult } from '@module/customer/account/domain/repository/customer/query/result/get-customer.query.result';
-import type { GetOrganizationQueryResult } from '@module/customer/account/domain/repository/organization/query/result/get-organization.query.result';
 import type { OrganizationMemberId } from '@module/customer/account/domain/schema/entity/organization-member/value-object/organization-member-id/organization-member-id.value-object';
 
 export class GetOrganizationMemberQueryResult extends BaseBuildableObject {
   public readonly id: OrganizationMemberId;
-  public readonly organization: GetOrganizationQueryResult;
-  public readonly customer: GetCustomerQueryResult;
   public readonly owner: boolean;
   public readonly createdAt: Date;
   public readonly updatedAt: Date;
