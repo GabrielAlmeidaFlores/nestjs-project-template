538c8e76fde6fad3c3fb2d4f776566255f4b0127
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-09-25 08:37:19 -0300

    :sparkles: add: set/organization

--- src/infra/database/implementation/typeorm/repository/organization-member/organization-member.typeorm.query.repository.ts
+++ src/infra/database/implementation/typeorm/repository/organization-member/organization-member.typeorm.query.repository.ts
@@ -7,6 +7,8 @@ import { OrganizationMemberTypeormEntity } from '@infra/database/implementation/
 import { MapperGateway } from '@lib/mapper/mapper.gateway';
 import { OrganizationMemberQueryRepositoryGateway } from '@module/customer/account/domain/repository/organization-member/query/organization-member.query.repository.gateway';
 import { GetOrganizationMemberQueryResult } from '@module/customer/account/domain/repository/organization-member/query/result/get-organization-member.query.result';
+import { CustomerId } from '@module/customer/account/domain/schema/entity/customer/value-object/customer-id/customer-id.value-object';
+import { OrganizationId } from '@module/customer/account/domain/schema/entity/organization/value-object/organization-id/organization-id.value-object';
 import { OrganizationMemberId } from '@module/customer/account/domain/schema/entity/organization-member/value-object/organization-member-id/organization-member-id.value-object';
 
 @Injectable()
@@ -22,6 +24,36 @@ export class OrganizationMemberTypeormQueryRepository
     private readonly mapperGateway: MapperGateway,
   ) {
     super(repository);
+  }
+
+  public async findOneOrganizationByCustomerAndOrganizationId(
+    customerId: CustomerId,
+    organizationId: OrganizationId,
+  ): Promise<GetOrganizationMemberQueryResult | null> {
+    const data = await this.findOne({
+      where: {
+        customer: {
+          id: customerId.toString(),
+        },
+        organization: {
+          id: organizationId.toString(),
+        },
+      },
+    });
+
+    const dataDoesNotExists = data === null;
+
+    if (dataDoesNotExists) {
+      return null;
+    }
+
+    const mappedData = this.mapperGateway.map(
+      data,
+      OrganizationMemberTypeormEntity,
+      GetOrganizationMemberQueryResult,
+    );
+
+    return mappedData;
   }
 
   public async findOneByOrganizationMemberId(
--- src/module/customer/account/account.module.ts
+++ src/module/customer/account/account.module.ts
@@ -4,18 +4,27 @@ import { BucketModule } from '@infra/bucket/bucket.module';
 import { DatabaseModule } from '@infra/database/database.module';
 import { AccountController } from '@module/customer/account/account.controller';
 import { FileProcessorModule } from '@module/customer/account/lib/file-processor/file-processor.module';
+import { OrganizationSessionModule } from '@module/customer/account/lib/organization-session/organization-session.module';
 import { CustomerSignUpUseCase } from '@module/customer/account/use-case/customer-sign-up.use-case';
 import { ListCustomerOrganizationsUseCase } from '@module/customer/account/use-case/list-customer-organizations.use-case';
+import { SetOrganizationForCustomerUseCase } from '@module/customer/account/use-case/set-organization-for-customer.use-case';
 import { UpdateCustomerProfilePictureUseCase } from '@module/customer/account/use-case/update-customer-profile-picture.use-case';
 import { AuthModule } from '@shared/api/gateway/guard/auth/auth.module';
 
 @Module({
-  imports: [AuthModule, DatabaseModule, BucketModule, FileProcessorModule],
+  imports: [
+    AuthModule,
+    DatabaseModule,
+    BucketModule,
+    FileProcessorModule,
+    OrganizationSessionModule,
+  ],
   controllers: [AccountController],
   providers: [
     CustomerSignUpUseCase,
     UpdateCustomerProfilePictureUseCase,
     ListCustomerOrganizationsUseCase,
+    SetOrganizationForCustomerUseCase,
   ],
 })
 export class AccountModule {
