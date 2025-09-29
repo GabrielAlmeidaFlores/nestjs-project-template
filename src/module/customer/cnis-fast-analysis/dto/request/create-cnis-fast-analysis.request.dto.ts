3340b5fb5e1b11b3fe7a77a491c1181094932612
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-09-29 11:41:55 -0300

    ✨ feat(CreateCnisFastAnalysisRequestDto):
    add parameters to client dto

--- src/module/customer/cnis-fast-analysis/dto/request/create-cnis-fast-analysis.request.dto.ts
+++ src/module/customer/cnis-fast-analysis/dto/request/create-cnis-fast-analysis.request.dto.ts
@@ -8,6 +8,7 @@ import { RequestDtoDateProperty } from '@shared/api/util/decorator/property/dto-
 import { RequestDtoEnumProperty } from '@shared/api/util/decorator/property/dto-property/request/request-dto-enum-property/request-dto-enum-property.decorator';
 import { MimeTypeEnum } from '@shared/api/util/decorator/property/dto-property/request/request-dto-file-property/enum/mime-type.enum';
 import { RequestDtoFileProperty } from '@shared/api/util/decorator/property/dto-property/request/request-dto-file-property/request-dto-file-property.decorator';
+import { RequestDtoNumberProperty } from '@shared/api/util/decorator/property/dto-property/request/request-dto-number-property/request-dto-number-property.decorator';
 import { RequestDtoObjectProperty } from '@shared/api/util/decorator/property/dto-property/request/request-dto-object-property/request-dto-object-property.decorator';
 import { RequestDtoStringProperty } from '@shared/api/util/decorator/property/dto-property/request/request-dto-string-property/request-dto-string-property.decorator';
 import { RequestDtoValueObjectProperty } from '@shared/api/util/decorator/property/dto-property/request/request-dto-value-object-property/request-dto-value-object-property.decorator';
@@ -33,6 +34,12 @@ export class CreateCnisFastAnalysisClientRequestDto extends BaseBuildableObject 
 
   @RequestDtoEnumProperty(GenderEnum, { required: false })
   public gender?: GenderEnum;
+
+  @RequestDtoNumberProperty({ required: false, isArray: true })
+  public legalProceedingNumber?: number[];
+
+  @RequestDtoNumberProperty({ required: false, isArray: true })
+  public inssBenefitNumber?: number[];
 
   @RequestDtoEnumProperty(QuickCnisAnalysisClientTypeEnum, { required: false })
   public clientType?: QuickCnisAnalysisClientTypeEnum;
