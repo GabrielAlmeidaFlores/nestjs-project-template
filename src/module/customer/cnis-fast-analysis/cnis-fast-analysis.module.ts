f632285f84d975d52f93b7d48555a5686bd35f17
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-10-02 14:35:19 -0300

    :zap: add: CnisIaAnalysisModule

--- src/module/customer/cnis-fast-analysis/cnis-fast-analysis.module.ts
+++ src/module/customer/cnis-fast-analysis/cnis-fast-analysis.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '@infra/database/database.module';
-import { GenerativeIaModule } from '@infra/generative-ia/generative-ia.module';
 import { CnisFastAnalysisController } from '@module/customer/cnis-fast-analysis/cnis-fast-analysis.controller';
+import { CnisIaAnalysisModule } from '@module/customer/cnis-fast-analysis/lib/cnis-ia-analysis/cnis-ia-analysis.module';
 import { FileProcessorModule } from '@module/customer/cnis-fast-analysis/lib/file-processor/file-processor.module';
 import { CreateCnisFastAnalysisResultUseCase } from '@module/customer/cnis-fast-analysis/use-case/create-cnis-fast-analysis-result.use-case';
 import { CreateCnisFastAnalysisUseCase } from '@module/customer/cnis-fast-analysis/use-case/create-cnis-fast-analysis.use-case';
@@ -18,7 +18,7 @@ import { OrganizationSessionModule } from '@shared/api/gateway/guard/organizatio
     DatabaseModule,
     OrganizationSessionModule,
     FileProcessorModule,
-    GenerativeIaModule,
+    CnisIaAnalysisModule,
   ],
   controllers: [CnisFastAnalysisController],
   providers: [
