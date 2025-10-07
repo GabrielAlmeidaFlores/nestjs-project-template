631e702891c3946116677aa9ce18fef02ac604b0
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-10-07 09:43:44 -0300

    :zap: add: implement CreateLegalPleadingAddressDataRequestDto and CreateLegalPleadingResponseDto classes

--- src/module/customer/analysis-tool/dto/request/create-legal-pleading.request.dto.ts
+++ src/module/customer/analysis-tool/dto/request/create-legal-pleading.request.dto.ts
@@ -1,4 +1,6 @@
+import { StateCodeEnum } from '@core/domain/schema/enum/state-code.enum';
 import { DecimalValue } from '@core/domain/schema/value-object/decimal/decimal.value-object';
+import { PostalCode } from '@core/domain/schema/value-object/postal-code/postal-code.value-object';
 import { AnalysisToolClientId } from '@module/customer/analysis-tool/domain/schema/entity/analysis-tool-client/value-object/analysis-tool-client-id/analysis-tool-client-id.value-object';
 import { LegalPleadingBenefitTypeEnum } from '@module/customer/analysis-tool/domain/schema/entity/legal-pleading/enum/legal-pleading-benefit-type.enum';
 import { LegalPleadingPetitionTypeEnum } from '@module/customer/analysis-tool/domain/schema/entity/legal-pleading/enum/legal-pleading-petition-type.enum';
@@ -11,11 +13,33 @@ import { RequestDtoDateProperty } from '@shared/api/util/decorator/property/dto-
 import { RequestDtoEnumProperty } from '@shared/api/util/decorator/property/dto-property/request/request-dto-enum-property/request-dto-enum-property.decorator';
 import { MimeTypeEnum } from '@shared/api/util/decorator/property/dto-property/request/request-dto-file-property/enum/mime-type.enum';
 import { RequestDtoFileProperty } from '@shared/api/util/decorator/property/dto-property/request/request-dto-file-property/request-dto-file-property.decorator';
+import { RequestDtoNumberProperty } from '@shared/api/util/decorator/property/dto-property/request/request-dto-number-property/request-dto-number-property.decorator';
 import { RequestDtoObjectProperty } from '@shared/api/util/decorator/property/dto-property/request/request-dto-object-property/request-dto-object-property.decorator';
 import { RequestDtoStringProperty } from '@shared/api/util/decorator/property/dto-property/request/request-dto-string-property/request-dto-string-property.decorator';
 import { RequestDtoValueObjectProperty } from '@shared/api/util/decorator/property/dto-property/request/request-dto-value-object-property/request-dto-value-object-property.decorator';
 import { BaseBuildableDtoObject } from '@shared/api/util/object/base-buildable-dto.object';
 import { FileModel } from '@shared/system/model/generic/file.model';
+
+@RequestDto()
+export class CreateLegalPleadingAddressDataRequestDto extends BaseBuildableDtoObject {
+  @RequestDtoStringProperty()
+  public city: string;
+
+  @RequestDtoStringProperty()
+  public neighborhood: string;
+
+  @RequestDtoEnumProperty(StateCodeEnum)
+  public stateCode: StateCodeEnum;
+
+  @RequestDtoValueObjectProperty(PostalCode)
+  public postalCode: PostalCode;
+
+  @RequestDtoNumberProperty()
+  public addressNumber: number;
+
+  protected override readonly _type =
+    CreateLegalPleadingAddressDataRequestDto.name;
+}
 
 @RequestDto()
 export class CreateLegalPleadingDataRequestDto extends BaseBuildableDtoObject {
@@ -63,6 +87,11 @@ export class CreateLegalPleadingDataRequestDto extends BaseBuildableDtoObject {
 
   @RequestDtoValueObjectProperty(AnalysisToolClientId)
   public analysisToolClientId: AnalysisToolClientId;
+
+  @RequestDtoObjectProperty(() => CreateLegalPleadingAddressDataRequestDto, {
+    required: false,
+  })
+  public legalPleadingAddress?: CreateLegalPleadingAddressDataRequestDto;
 
   protected override readonly _type = CreateLegalPleadingDataRequestDto.name;
 }
