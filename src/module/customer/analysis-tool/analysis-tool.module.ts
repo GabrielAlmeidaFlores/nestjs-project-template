dc2e555b1130e5a64a085202a495c720cd3bf74c
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-10-09 15:13:13 -0300

    feature: adds endpoint for downloading the AI analysis of the CNIS document

--- src/module/customer/analysis-tool/analysis-tool.module.ts
+++ src/module/customer/analysis-tool/analysis-tool.module.ts
@@ -3,12 +3,14 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@infra/database/database.module';
 import { AnalysisToolController } from '@module/customer/analysis-tool/analysis-tool.controller';
 import { DocumentAnalysisModule } from '@module/customer/analysis-tool/lib/document-analysis/document-analysis.module';
+import { ExportDocumentModule } from '@module/customer/analysis-tool/lib/export-document/export-document.module';
 import { FileProcessorModule } from '@module/customer/analysis-tool/lib/file-processor/file-processor.module';
 import { CreateAnalysisToolClientUseCase } from '@module/customer/analysis-tool/use-case/create-analysis-tool-client.use-case';
 import { CreateCnisFastAnalysisResultUseCase } from '@module/customer/analysis-tool/use-case/create-cnis-fast-analysis-result.use-case';
 import { CreateCnisFastAnalysisUseCase } from '@module/customer/analysis-tool/use-case/create-cnis-fast-analysis.use-case';
 import { DeleteAnalysisToolClientUseCase } from '@module/customer/analysis-tool/use-case/delete-analysis-tool-client.use-case';
 import DeleteCnisFastAnalysisUseCase from '@module/customer/analysis-tool/use-case/delete-cnis-fast-analysis.use-case';
+import DownloadCnisFastAnalysisUseCase from '@module/customer/analysis-tool/use-case/download-cnis-fast-analysis.use-case';
 import { GetCnisFastAnalysisUseCase } from '@module/customer/analysis-tool/use-case/get-cnis-fast-analysis.use-case';
 import { ListAnalysisToolClientUseCase } from '@module/customer/analysis-tool/use-case/list-analysis-tool-client.use-case';
 import { ListCnisFastAnalysisUseCase } from '@module/customer/analysis-tool/use-case/list-cnis-fast-analysis.use-case';
@@ -22,6 +24,7 @@ import { OrganizationSessionModule } from '@shared/api/gateway/guard/organizatio
     OrganizationSessionModule,
     FileProcessorModule,
     DocumentAnalysisModule,
+    ExportDocumentModule,
   ],
   controllers: [AnalysisToolController],
   providers: [
@@ -34,6 +37,7 @@ import { OrganizationSessionModule } from '@shared/api/gateway/guard/organizatio
     CreateAnalysisToolClientUseCase,
     DeleteCnisFastAnalysisUseCase,
     DeleteAnalysisToolClientUseCase,
+    DownloadCnisFastAnalysisUseCase,
   ],
 })
 export class AnalysisToolModule {
--- src/module/customer/analysis-tool/lib/export-document/export-document.gateway.ts
+++ src/module/customer/analysis-tool/lib/export-document/export-document.gateway.ts
@@ -1,1 +1,14 @@
-export abstract class ExportDocumentGateway {}
+import type { ExportDocumentFormatEnum } from '@module/customer/analysis-tool/lib/enum/export-document-type.enum';
+import type { StreamableFile } from '@nestjs/common';
+
+export abstract class ExportDocumentGateway {
+  public abstract downloadFile(
+    content: string,
+    format: ExportDocumentFormatEnum,
+  ): Promise<Buffer>;
+
+  public abstract downloadFileAsStreamable(
+    content: string,
+    format: ExportDocumentFormatEnum,
+  ): Promise<StreamableFile>;
+}
