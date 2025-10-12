8edb2489baefbfdbff9c6ccca1b97547fcd81b4d
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-10-12 09:47:46 -0300

    feat: refactor DTOs for analysis tool records and update imports

--- src/module/customer/analysis-tool/dto/response/list-analysis-tool-record.response.dto.ts
+++ src/module/customer/analysis-tool/dto/response/list-analysis-tool-record.response.dto.ts
@@ -1,4 +1,4 @@
-import { GetAnalysisToolRecordResponseDto } from '@module/customer/analysis-tool/dto/response/get-analysis-tool-record.request.dto';
+import { GetAnalysisToolRecordResponseDto } from '@module/customer/analysis-tool/dto/response/get-analysis-tool-record.response.dto';
 import { ResponseDto } from '@shared/api/util/decorator/class/dto-specification/response-dto.decorator';
 import { ResponseDtoObjectProperty } from '@shared/api/util/decorator/property/dto-property/response/response-dto-object-property/response-dto-object-property.decorator';
 import { ListDataResponseDto } from '@shared/api/util/dto/response/list-data.response.dto';
