43fbedf40bb58ce6397125630054b844a0d773a2
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-10-13 06:12:34 -0300

    feat: add LoremIpsum module and service, update generative IA module, and standardize enum values

--- src/infra/generative-ia/generative-ia.module.ts
+++ src/infra/generative-ia/generative-ia.module.ts
@@ -3,15 +3,21 @@ import { Module } from '@nestjs/common';
 import { GenerativeIaGateway } from '@infra/generative-ia/generative-ia.gateway';
 import { GeminiModule } from '@infra/generative-ia/implementation/geminini/gemini.module';
 import { GeminiService } from '@infra/generative-ia/implementation/geminini/gemini.service';
+import { LoremIpsumModule } from '@infra/generative-ia/implementation/lorem-ipsum/lorem-ipsum.module';
+import { LoremIpsumService } from '@infra/generative-ia/implementation/lorem-ipsum/lorem-ipsum.service';
+import { NodeApplicationVariable } from '@shared/system/constant/application-variable/source/node.application-variable';
 
 @Module({
-  imports: [GeminiModule],
+  imports: [GeminiModule, LoremIpsumModule],
   providers: [
     {
       provide: GenerativeIaGateway,
-      useClass: GeminiService,
+      useClass: NodeApplicationVariable.PRODUCTION_ENVIRONMENT
+        ? GeminiService
+        : LoremIpsumService,
     },
     GeminiService,
+    LoremIpsumService,
   ],
   exports: [GenerativeIaGateway],
 })
