53bf498a2fc571d158ede0147b96fc4dc8802cee
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-10-14 09:16:00 -0300

    feat: implement legal pleading document analysis use case and related DTOs

--- src/module/customer/analysis-tool/lib/file-processor/file-processor.gateway.ts
+++ src/module/customer/analysis-tool/lib/file-processor/file-processor.gateway.ts
@@ -1,14 +1,14 @@
 import type { FileModel } from '@shared/system/model/generic/file.model';
 
 export abstract class FileProcessorGateway {
-  public abstract uploadDocument(
+  public abstract uploadFile(
     document: FileModel,
     documentLocation?: string,
   ): Promise<string>;
 
   public abstract getOriginalFileName(fileName: string): Promise<string>;
 
-  public abstract getDocumentBuffer(fileName: string): Promise<Buffer>;
+  public abstract getFileBuffer(fileName: string): Promise<Buffer>;
 
   public abstract getFileSignedUrl(fileName: string): Promise<URL>;
 }
