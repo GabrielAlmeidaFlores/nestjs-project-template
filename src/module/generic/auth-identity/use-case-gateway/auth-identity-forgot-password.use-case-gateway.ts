89ca8daf6a55a016dde56b92ce4ec23467ef7a35
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-10-17 17:33:45 -0300

    refactor: remove AuthIdentityForgotPasswordResponseDto and related gateway, update forgotPassword method to return void

--- src/module/generic/auth-identity/use-case-gateway/auth-identity-forgot-password.use-case-gateway.ts
+++
@@ -1,7 +0,0 @@
-import type { AuthIdentityForgotPasswordRequestDto } from '@module/generic/auth-identity/dto/request/auth-identity-forgot-password.request.dto';
-import type { AuthIdentityForgotPasswordResponseDto } from '@module/generic/auth-identity/dto/response/auth-identity-forgot-password.response.dto';
-export abstract class AuthIdentityForgotPasswordUseCaseGateway {
-  public abstract execute(
-    dto: AuthIdentityForgotPasswordRequestDto,
-  ): Promise<AuthIdentityForgotPasswordResponseDto>;
-}
