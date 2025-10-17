32b803ea800934a7f361f33b282d7e70a8d9c26c
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-10-17 15:59:10 -0300

    refactor: simplify DateTransformer by removing utility class and direct method calls

--- src/infra/database/implementation/typeorm/schema/transformer/date.transformer.ts
+++ src/infra/database/implementation/typeorm/schema/transformer/date.transformer.ts
@@ -1,23 +1,11 @@
-class DateTransformerUtil {
-  protected readonly _type = DateTransformerUtil.name;
-
-  public static encrypt(value?: string): string | undefined {
+export const DateTransformer = {
+  to(value?: string): string | undefined {
     return value;
-  }
-
-  public static decrypt(value?: string): Date | undefined {
+  },
+  from(value?: string): Date | undefined {
     if (value === undefined) {
       return;
     }
     return new Date(value);
-  }
-}
-
-export const DateTransformer = {
-  to(value?: string): string | undefined {
-    return DateTransformerUtil.encrypt(value);
-  },
-  from(value?: string): Date | undefined {
-    return DateTransformerUtil.decrypt(value);
   },
 };
