0eb6e62ee2bdbec31071a6e5f68fc7909097be51
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-10-17 15:24:30 -0300

    refactor: rename terms acceptance use cases and related DTOs for consistency

--- src/module/customer/account/use-case/get-customer-terms-acceptance.use-case.ts
+++ src/module/customer/account/use-case/get-customer-terms-acceptance.use-case.ts
@@ -1,30 +1,21 @@
 import { Inject } from '@nestjs/common';
 
-import { BaseTransactionRepositoryGateway } from '@core/domain/repository/base/transaction/base.transaction.repository.gateway';
 import { CustomerQueryRepositoryGateway } from '@module/customer/account/domain/repository/customer/query/customer.query.repository.gateway';
 import { CustomerTermsQueryRepositoryGateway } from '@module/customer/account/domain/repository/customer-terms/query/customer-terms.query.repository.gateway';
-import { CustomerTermsAcceptanceCommandRepositoryGateway } from '@module/customer/account/domain/repository/customer-terms-acceptance/command/customer-terms-acceptance.command.repository.gateway';
 import { CustomerTermsAcceptanceQueryRepositoryGateway } from '@module/customer/account/domain/repository/customer-terms-acceptance/query/customer-terms-acceptance.query.repository.gateway';
 import { OrganizationMemberQueryRepositoryGateway } from '@module/customer/account/domain/repository/organization-member/query/organization-member.query.repository.gateway';
-import { CustomerEntity } from '@module/customer/account/domain/schema/entity/customer/customer.entity';
-import { CustomerAddressEntity } from '@module/customer/account/domain/schema/entity/customer-address/customer-address.entity';
-import { CustomerTermsEntity } from '@module/customer/account/domain/schema/entity/customer-terms/customer-terms.entity';
-import { CustomerTermsAcceptanceEntity } from '@module/customer/account/domain/schema/entity/customer-terms-acceptance/customer-terms-acceptance.entity';
-import { CustomerTermsAcceptanceResponseDto } from '@module/customer/account/dto/response/customer-terms-acceptance.response.dto';
+import { GetCustomerTermsAcceptanceDataResponseDto } from '@module/customer/account/dto/response/get-terms-acceptance-data.response.dto';
 import { CustomerNotFoundError } from '@module/customer/account/error/customer-not-found-error.error';
-import { CustomerTermsAcceptanceError } from '@module/customer/account/error/customer-terms-acceptance.error';
 import { CustomerTermsNotFoundError } from '@module/customer/account/error/customer-terms-not-found.error';
 import { InvalidOrganizationSessionError } from '@module/customer/account/error/invalid-organization-session.error';
 
 import type { OrganizationSessionDataModel } from '@shared/api/util/decorator/property/get-organization-session-data/model/generic/organization-session-data.model';
 import type { SessionDataModel } from '@shared/api/util/decorator/property/get-session-data/model/generic/session-data.model';
 
-export class ConfirmTermsAcceptanceUseCase {
-  protected readonly _type = ConfirmTermsAcceptanceUseCase.name;
+export class GetCustomerTermsAcceptanceUseCase {
+  protected readonly _type = GetCustomerTermsAcceptanceUseCase.name;
 
   public constructor(
-    @Inject(BaseTransactionRepositoryGateway)
-    private readonly baseTransactionRepositoryGateway: BaseTransactionRepositoryGateway,
     @Inject(CustomerQueryRepositoryGateway)
     private readonly customerQueryRepositoryGateway: CustomerQueryRepositoryGateway,
     @Inject(OrganizationMemberQueryRepositoryGateway)
@@ -33,22 +24,21 @@ export class ConfirmTermsAcceptanceUseCase {
     private readonly customerTermsQueryRepositoryGateway: CustomerTermsQueryRepositoryGateway,
     @Inject(CustomerTermsAcceptanceQueryRepositoryGateway)
     private readonly customerTermsAcceptanceQueryRepositoryGateway: CustomerTermsAcceptanceQueryRepositoryGateway,
-    @Inject(CustomerTermsAcceptanceCommandRepositoryGateway)
-    private readonly customerTermsAcceptanceCommandRepositoryGateway: CustomerTermsAcceptanceCommandRepositoryGateway,
   ) {}
 
   public async execute(
     sessionData: SessionDataModel,
     organizationSessionData: OrganizationSessionDataModel,
-  ): Promise<CustomerTermsAcceptanceResponseDto> {
-    const customerResult =
-      await this.customerQueryRepositoryGateway.findOneByAuthIdentityIdWithCustomerAddressRelationOrFail(
+  ): Promise<GetCustomerTermsAcceptanceDataResponseDto> {
+    const customer =
+      await this.customerQueryRepositoryGateway.findOneByAuthIdentityIdOrFail(
         sessionData.authIdentityId,
         CustomerNotFoundError,
       );
+
     const organizationMember =
       await this.organizationMemberQueryRepositoryGateway.findOneByCustomerAndOrganizationIdWithRelations(
-        customerResult.id,
+        customer.id,
         organizationSessionData.organizationId,
       );
 
@@ -56,52 +46,24 @@ export class ConfirmTermsAcceptanceUseCase {
       throw new InvalidOrganizationSessionError();
     }
 
-    const termResult =
+    const terms =
       await this.customerTermsQueryRepositoryGateway.findOneByStatus(true);
 
-    if (termResult === null) {
+    if (terms === null) {
       throw new CustomerTermsNotFoundError();
     }
 
     const customerTermsAcceptance =
       await this.customerTermsAcceptanceQueryRepositoryGateway.findOneByTermsIdAndCustomerId(
-        termResult.id,
-        customerResult.id,
+        terms.id,
+        customer.id,
       );
 
     const accepted = customerTermsAcceptance !== null;
 
-    if (accepted) {
-      throw new CustomerTermsAcceptanceError();
-    }
-
-    const customer = new CustomerEntity({
-      ...customerResult,
-      customerAddress: new CustomerAddressEntity({
-        ...customerResult.customerAddress,
-      }),
-    });
-
-    const customerTerms = new CustomerTermsEntity({ ...termResult });
-
-    const data = new CustomerTermsAcceptanceEntity({
-      customer,
-      customerTerms,
-    });
-
-    const createCustomerTermsAcceptance =
-      this.customerTermsAcceptanceCommandRepositoryGateway.createCustomerTermsAcceptance(
-        data,
-      );
-
-    const transaction = await this.baseTransactionRepositoryGateway.execute([
-      createCustomerTermsAcceptance,
-    ]);
-
-    await transaction.commit();
-
-    return CustomerTermsAcceptanceResponseDto.build({
-      customerTermsAcceptance: data.id,
+    return GetCustomerTermsAcceptanceDataResponseDto.build({
+      content: terms.content,
+      accepted,
     });
   }
 }
