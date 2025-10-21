4e95508e5661838e6a6442865cbac67aa57651e3
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-10-21 13:09:21 -0300

    feature: adds endpoint to edit a customer added for analysis

--- src/infra/database/implementation/typeorm/schema/entity/customer.typeorm.entity.ts
+++ src/infra/database/implementation/typeorm/schema/entity/customer.typeorm.entity.ts
@@ -3,9 +3,7 @@ import { Column, Entity, JoinColumn, OneToMany, OneToOne } from 'typeorm';
 import { AuthIdentityTypeormEntity } from '@infra/database/implementation/typeorm/schema/entity/auth-identity.typeorm.entity';
 import { BaseTypeormEntity } from '@infra/database/implementation/typeorm/schema/entity/base.typeorm.entity';
 import { CustomerAddressTypeormEntity } from '@infra/database/implementation/typeorm/schema/entity/customer-address.typeorm.entity';
-import {
-  CustomerTermsAcceptanceTypeormEntity
-} from '@infra/database/implementation/typeorm/schema/entity/customer-terms-acceptance.typeorm.entity';
+import { CustomerTermsAcceptanceTypeormEntity } from '@infra/database/implementation/typeorm/schema/entity/customer-terms-acceptance.typeorm.entity';
 import { OrganizationMemberTypeormEntity } from '@infra/database/implementation/typeorm/schema/entity/organization-member.typeorm.entity';
 
 @Entity({ name: 'customer' })
