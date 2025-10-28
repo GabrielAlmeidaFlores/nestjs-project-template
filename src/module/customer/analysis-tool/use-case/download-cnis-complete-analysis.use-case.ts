d337b86b95d22bc87f7524a0dc815cf93af316cf
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-10-28 11:45:54 -0300

    feat: refactor export document enum and update imports across use cases

--- src/module/customer/analysis-tool/use-case/download-cnis-complete-analysis.use-case.ts
+++ src/module/customer/analysis-tool/use-case/download-cnis-complete-analysis.use-case.ts
@@ -6,7 +6,7 @@ import { CnisFastAnalysisId } from '@module/customer/analysis-tool/domain/schema
 import { CnisFastAnalysisDoesNotContainCompleteAnalysisError } from '@module/customer/analysis-tool/error/cnis-fast-analysis-does-not-contain-complete-analysis.error';
 import { CnisFastAnalysisNotFoundError } from '@module/customer/analysis-tool/error/cnis-fast-analysis-not-found.error';
 import { OrganizationMemberNotFoundError } from '@module/customer/analysis-tool/error/organization-member-not-found-error.error';
-import { ExportDocumentFormatEnum } from '@module/customer/analysis-tool/lib/enum/export-document-type.enum';
+import { ExportDocumentFormatEnum } from '@module/customer/analysis-tool/lib/export-document/enum/export-document-type.enum';
 import { ExportDocumentGateway } from '@module/customer/analysis-tool/lib/export-document/export-document.gateway';
 import { OrganizationSessionDataModel } from '@shared/api/util/decorator/property/get-organization-session-data/model/generic/organization-session-data.model';
 import { SessionDataModel } from '@shared/api/util/decorator/property/get-session-data/model/generic/session-data.model';
