3f56978aefc9c02f0cd4f4e56cdee4fd7f249cd1
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-11-03 14:45:16 -0300

    :hammer: refactor: replace DeepPartialType with Partial in constructor parameters for consistency

--- src/module/customer/analysis-tool/domain/repository/analysis-tool-record/query/param/list-analysis-tool-record.query.param.ts
+++ src/module/customer/analysis-tool/domain/repository/analysis-tool-record/query/param/list-analysis-tool-record.query.param.ts
@@ -1,8 +1,7 @@
 import { ListDataInputModel } from '@core/domain/repository/base/query/model/input/list-data.input.model';
-import { AnalysisToolClientId } from '@module/customer/analysis-tool/domain/schema/entity/analysis-tool-client/value-object/analysis-tool-client-id/analysis-tool-client-id.value-object';
 
+import type { AnalysisToolClientId } from '@module/customer/analysis-tool/domain/schema/entity/analysis-tool-client/value-object/analysis-tool-client-id/analysis-tool-client-id.value-object';
 import type { AnalysisToolRecordTypeEnum } from '@module/customer/analysis-tool/domain/schema/entity/analysis-tool-record/enum/analysis-tool-record-type.enum';
-import type { DeepPartialType } from '@shared/system/type/deep-partial.type';
 
 export class ListAnalysisToolRecordQueryParam extends ListDataInputModel {
   public readonly type: AnalysisToolRecordTypeEnum | null;
@@ -11,16 +10,11 @@ export class ListAnalysisToolRecordQueryParam extends ListDataInputModel {
 
   protected override readonly _type = ListAnalysisToolRecordQueryParam.name;
 
-  public constructor(props: DeepPartialType<ListAnalysisToolRecordQueryParam>) {
+  public constructor(props: Partial<ListAnalysisToolRecordQueryParam>) {
     super(props);
 
     this.type = props.type ?? null;
     this.searchBy = props.searchBy ?? null;
-    this.analysisToolClientId =
-      props.analysisToolClientId instanceof AnalysisToolClientId
-        ? props.analysisToolClientId
-        : props.analysisToolClientId
-          ? new AnalysisToolClientId(props.analysisToolClientId as string)
-          : null;
+    this.analysisToolClientId = props.analysisToolClientId ?? null;
   }
 }
