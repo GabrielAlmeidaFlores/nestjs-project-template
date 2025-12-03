09616eeb9d253659bdae3e2657be0e72329e04ef
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-12-03 12:35:42 -0300

    feat: adds module for administrator with use case to retrieve registration details

---
+++ src/module/admin/account/error/admin-not-found.error.ts
@@ -0,0 +1,9 @@
+import { NotFoundError } from '@core/error/not-found.error';
+
+export class AdminNotFoundError extends NotFoundError {
+  protected override readonly _type = AdminNotFoundError.name;
+
+  public constructor() {
+    super('Admin nao encontrado');
+  }
+}
