dafbe4d38ceef4eaf5924cda07d405299ca15f1c
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-12-05 14:40:49 -0300

    fix: update parameter reference for CNIS document analysis in CreateCnisFastAnalysisResultUseCase

--- src/module/customer/analysis-tool/use-case/create-cnis-fast-analysis-result.use-case.ts
+++ src/module/customer/analysis-tool/use-case/create-cnis-fast-analysis-result.use-case.ts
@@ -94,7 +94,7 @@ export class CreateCnisFastAnalysisResultUseCase {
     const cnisAnalyzerResponse =
       await this.cnisAnalysisGateway.analyzeCnisDocument(
         cnisDocumentData,
-        cnisFastAnalysisQueryResult.analysisToolClient,
+        analysisToolRecordQueryResult.analysisToolClient,
       );
 
     const jsonCnisAnalyzerResponse = JSON.stringify(
