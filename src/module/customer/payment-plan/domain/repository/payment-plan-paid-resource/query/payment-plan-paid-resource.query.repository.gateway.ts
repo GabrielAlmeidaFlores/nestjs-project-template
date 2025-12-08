324b6441208a52b38d5b4e77e1b8383562f09d70
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-12-08 15:19:14 -0300

    feat: implement PaymentPlanPaidResourceIaConfig command and query repositories with related entities

--- src/module/customer/payment-plan/domain/repository/payment-plan-paid-resource/query/payment-plan-paid-resource.query.repository.gateway.ts
+++ src/module/customer/payment-plan/domain/repository/payment-plan-paid-resource/query/payment-plan-paid-resource.query.repository.gateway.ts
@@ -1,17 +1,17 @@
 import type { NotFound } from '@aws-sdk/client-s3';
 import type { ListDataInputModel } from '@core/domain/repository/base/query/model/input/list-data.input.model';
 import type { ListDataOutputModel } from '@core/domain/repository/base/query/model/output/list-data.output.model';
-import type { GetPaymentPlayPaidResourceQueryResult } from '@module/customer/payment-plan/domain/repository/payment-plan-paid-resource/query/result/get-payment-plan-paid-resource.query.results';
+import type { GetPaymentPlanPaidResourceQueryResult } from '@module/customer/payment-plan/domain/repository/payment-plan-paid-resource/query/result/get-payment-plan-paid-resource.query.results';
 import type { PaymentPlanPaidResourceId } from '@module/customer/payment-plan/domain/schema/entity/payment-plan-paid-resource/value-object/payment-plan-paid-resource-id/payment-plan-paid-resource-id.value.object';
 import type { Constructor } from 'type-fest';
 
 export abstract class PaymentPlanPaidResourceQueryRepositoryGateway {
   public abstract listPaymentPlanPaidResource(
     listData: ListDataInputModel,
-  ): Promise<ListDataOutputModel<GetPaymentPlayPaidResourceQueryResult>>;
+  ): Promise<ListDataOutputModel<GetPaymentPlanPaidResourceQueryResult>>;
 
-  public abstract findOnePaymentPlanPaidResource(
+  public abstract findOnePaymentPlanPaidResourceByIdOrFail(
     id: PaymentPlanPaidResourceId,
     err: Constructor<NotFound>,
-  ): Promise<GetPaymentPlayPaidResourceQueryResult>;
+  ): Promise<GetPaymentPlanPaidResourceQueryResult>;
 }
