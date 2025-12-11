fa33c511ed218c1f758d7957866e737364d96920
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-12-11 15:53:35 -0300

    feat: integration of the endpoint to list legal proceeding details

--- src/module/customer/legal-proceeding/domain/repository/legal-proceeding-detail/query/legal-proceeding-detail.query.repository.gateway.ts
+++ src/module/customer/legal-proceeding/domain/repository/legal-proceeding-detail/query/legal-proceeding-detail.query.repository.gateway.ts
@@ -1,7 +1,18 @@
+import type { ListDataOutputModel } from '@core/domain/repository/base/query/model/output/list-data.output.model';
+import type { OrganizationId } from '@module/customer/account/domain/schema/entity/organization/value-object/organization-id/organization-id.value-object';
+import type { ListLegalProceedingDetailQueryParam } from '@module/customer/legal-proceeding/domain/repository/legal-proceeding-detail/query/param/list-legal-proceeding-detail.query.param.gateway';
+import type { GetLegalProceedingDetailWithRelationsQueryResult } from '@module/customer/legal-proceeding/domain/repository/legal-proceeding-detail/query/result/get-legal-proceeding-detail-with-relations.query.result';
 import type { GetLegalProceedingDetailQueryResult } from '@module/customer/legal-proceeding/domain/repository/legal-proceeding-detail/query/result/get-legal-proceeding-detail.query.result';
 
 export abstract class LegalProceedingDetailQueryRepositoryGateway {
   public abstract findOneByLegalProceeding(
     legalProceedingNumber: string,
   ): Promise<GetLegalProceedingDetailQueryResult>;
+
+  public abstract listByOrganizationIdAndAnalysisToolClientId(
+    organizationId: OrganizationId,
+    listData: ListLegalProceedingDetailQueryParam,
+  ): Promise<
+    ListDataOutputModel<GetLegalProceedingDetailWithRelationsQueryResult>
+  >;
 }
---
+++ src/module/customer/legal-proceeding/dto/response/list-legal-proceeding-detail.response.dto.ts
@@ -0,0 +1,14 @@
+import { GetLegalProceedingDetailResponseDto } from '@module/customer/legal-proceeding/dto/response/get-legal-proceeding-detail.response.dto';
+import { ResponseDto } from '@shared/api/util/decorator/class/dto-specification/response-dto.decorator';
+import { ResponseDtoObjectProperty } from '@shared/api/util/decorator/property/dto-property/response/response-dto-object-property/response-dto-object-property.decorator';
+import { ListDataResponseDto } from '@shared/api/util/dto/response/list-data.response.dto';
+
+@ResponseDto()
+export class ListLegalProceedingDetailResponseDto extends ListDataResponseDto<GetLegalProceedingDetailResponseDto> {
+  @ResponseDtoObjectProperty(() => GetLegalProceedingDetailResponseDto, {
+    isArray: true,
+  })
+  public override resource: GetLegalProceedingDetailResponseDto[];
+
+  protected override readonly _type = ListLegalProceedingDetailResponseDto.name;
+}
