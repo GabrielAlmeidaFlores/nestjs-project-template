2ff2e49a2803f372b7580f126ac80a7275081406
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-12-15 07:53:51 -0300

    refactor: update organization and payment plan properties to use value objects

--- src/module/customer/payment-plan/domain/schema/entity/organization-payment-plan/organization-payment-plan.entity.ts
+++ src/module/customer/payment-plan/domain/schema/entity/organization-payment-plan/organization-payment-plan.entity.ts
@@ -1,9 +1,9 @@
 import { BaseEntity } from '@core/domain/schema/entity/base/base.entity';
 import { DecimalValue } from '@core/domain/schema/value-object/decimal/decimal.value-object';
-import { OrganizationEntity } from '@module/customer/account/domain/schema/entity/organization/organization.entity';
+import { OrganizationId } from '@module/customer/account/domain/schema/entity/organization/value-object/organization-id/organization-id.value-object';
 import { OrganizationPaymentPlanEntityPropsInterface } from '@module/customer/payment-plan/domain/schema/entity/organization-payment-plan/organization-payment-plan.entity.props.interface';
 import { OrganizationPaymentPlanId } from '@module/customer/payment-plan/domain/schema/entity/organization-payment-plan/value-object/organization-payment-plan-id/organization-payment-plan-id.value-object';
-import { PaymentPlanEntity } from '@module/customer/payment-plan/domain/schema/entity/payment-plan/payment-plan.entity';
+import { PaymentPlanId } from '@module/customer/payment-plan/domain/schema/entity/payment-plan/value-object/payment-plan-id/payment-plan-id.value-object';
 import { PaymentPlanCycleEnum } from '@module/customer/payment-plan/domain/schema/enum/payment-plan-cycle.enum';
 import { Description } from '@shared/system/decorator/property/description/description.decorator';
 
@@ -30,10 +30,10 @@ export class OrganizationPaymentPlanEntity extends BaseEntity<OrganizationPaymen
   public readonly cycle: PaymentPlanCycleEnum;
 
   @Description('Organização pertencente ao plano de pagamento')
-  public readonly organization: OrganizationEntity | null;
+  public readonly organization: OrganizationId;
 
-  @Description('Plano de pagamento vinculado a organização')
-  public readonly paymentPlan: PaymentPlanEntity | null;
+  @Description('Identificador do plano de pagamento')
+  public readonly paymentPlan: PaymentPlanId;
 
   protected readonly _type = OrganizationPaymentPlanEntity.name;
 
@@ -47,7 +47,7 @@ export class OrganizationPaymentPlanEntity extends BaseEntity<OrganizationPaymen
     this.monthlyCreditAmount = props.monthlyCreditAmount;
     this.active = props.active;
     this.cycle = props.cycle;
-    this.organization = props.organization ?? null;
-    this.paymentPlan = props.paymentPlan ?? null;
+    this.organization = props.organization;
+    this.paymentPlan = props.paymentPlan;
   }
 }
