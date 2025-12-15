bc7ac2de34b0b1180339fff007ec2dd690b98b9d
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-12-15 06:13:30 -0300

    feat: add AdminSeeder to seed module and update SeedService to include it

--- src/cli/seed/seed.module.ts
+++ src/cli/seed/seed.module.ts
@@ -1,12 +1,13 @@
 import { Logger, Module } from '@nestjs/common';
 
 import { SeedService } from '@cli/seed/seed.service';
+import { AdminSeeder } from '@cli/seed/seeder/admin.seeder';
 import { CustomerTermsSeeder } from '@cli/seed/seeder/customer-terms.seeder';
 import { DatabaseModule } from '@infra/database/database.module';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [SeedService, CustomerTermsSeeder, Logger],
+  providers: [SeedService, AdminSeeder, CustomerTermsSeeder, Logger],
 })
 export class SeedModule {
   protected readonly _type = SeedModule.name;
