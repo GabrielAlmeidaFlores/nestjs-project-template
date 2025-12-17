4b447941dd6fc03a59f17ce4d2a391952eebac41
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-12-17 11:51:35 -0300

    feat: add watch mode for development and refactor retirement planning use case

--- src/module/customer/analysis-tool/use-case/create-retirement-planning-rgps-cnis.ts
+++ src/module/customer/analysis-tool/use-case/create-retirement-planning-rgps-cnis.ts
@@ -1,8 +1,12 @@
 import { Inject, Injectable } from '@nestjs/common';
 
 import { BaseTransactionRepositoryGateway } from '@core/domain/repository/base/transaction/base.transaction.repository.gateway';
+import { RetirementPlanningRgpsPeriodCommandRepositoryGateway } from '@module/customer/analysis-tool/domain/repository/retirement-planning-rgps-period/command/retirement-planning-rgps-period.repository.gateway';
 import { RetirementPlanningRgpsCommandRepositoryGateway } from '@module/customer/analysis-tool/domain/repository/retirement-planning-rgps/command/retirement-planning-rgps.repository.gateway';
 import { RetirementPlanningRgpsQueryRepositoryGateway } from '@module/customer/analysis-tool/domain/repository/retirement-planning-rgps/query/retirement-planning-rgps.query.repository.gateway';
+import { RetirementPlanningRgpsPeriodEntity } from '@module/customer/analysis-tool/domain/schema/entity/retirement-planning-rgps-period/retirement-planning-rgps-period.entity';
+import { RetirementPlanningRgpsEntity } from '@module/customer/analysis-tool/domain/schema/entity/retirement-planning-rgps/retirement-planning-rgps.entity';
+import { RetirementPlanningRgpsId } from '@module/customer/analysis-tool/domain/schema/entity/retirement-planning-rgps/value-object/retirement-planning-rgps-id.value-object';
 import { CreateRetirementPlanningRgpsCnisRequestDto } from '@module/customer/analysis-tool/dto/request/create-retirement-planning-rgps-cnis.request.dto';
 import { CreateRetirementPlanningRgpsCnisResponseDto } from '@module/customer/analysis-tool/dto/response/create-retirement-planning-rgps-cnis.response.dto';
 import { CnisDocumentIsNotValidError } from '@module/customer/analysis-tool/error/cnis-document-is-not-valid.error';
@@ -23,6 +27,8 @@ export class CreateRetirementPlanningRgpsCnisUseCase {
     private readonly analysisProcessorGateway: AnalysisProcessorGateway,
     @Inject(RetirementPlanningRgpsCommandRepositoryGateway)
     private readonly retirementPlanningRgpsCommandRepositoryGateway: RetirementPlanningRgpsCommandRepositoryGateway,
+    @Inject(RetirementPlanningRgpsPeriodCommandRepositoryGateway)
+    private readonly retirementPlanningRgpsPeriodCommandRepositoryGateway: RetirementPlanningRgpsPeriodCommandRepositoryGateway,
     @Inject(FileProcessorGateway)
     private readonly fileProcessorGateway: FileProcessorGateway,
   ) {}
@@ -51,31 +57,72 @@ export class CreateRetirementPlanningRgpsCnisUseCase {
           ? await this.fileProcessorGateway.uploadFile(dto.cnisDocument)
           : null;
 
-      // const updatedRetirementPlanningRgps = new RetirementPlanningRgpsEntity({
-      //   ...retirementPlanningRgps,
+      const updatedRetirementPlanningRgps = new RetirementPlanningRgpsEntity({
+        ...retirementPlanningRgps,
+        cnisDocument,
+      });
 
-      //   cnisDocument,
-      // });
+      const cnis = await this.analysisProcessorGateway.parseCnisDocument(
+        dto.cnisDocument.buffer,
+      );
 
-      // const retirementPlanningRgpsTransaction =
-      //   this.retirementPlanningRgpsCommandRepositoryGateway.updateRetirementPlanningRgps(
-      //     retirementPlanningRgps.id,
-      //     updatedRetirementPlanningRgps,
-      //   );
+      const periods =
+        cnis?.socialSecurityRelations !== undefined
+          ? cnis?.socialSecurityRelations.map((relation) => {
+              return new RetirementPlanningRgpsPeriodEntity({
+                periodName:
+                  relation.socialSecurityAffiliationInfo.origemDoVinculo ??
+                  null,
+                periodStart:
+                  relation.socialSecurityAffiliationInfo.dataInicio ?? null,
+                periodEnd:
+                  relation.socialSecurityAffiliationInfo.dataFim ?? null,
+                category:
+                  relation.socialSecurityAffiliationInfo.tipoFiliadoNoVinculo ??
+                  null,
+                isPendency: true,
+                competenceBelowTheMinimum: true,
+                contributionAverage: 0,
+                typeOfContribution: 'Urbano',
+                retirementPlanningRgps: updatedRetirementPlanningRgps,
+              });
+            })
+          : [];
 
-      // const transactions = await this.baseTransactionRepositoryGateway.execute([
-      //   retirementPlanningRgpsTransaction,
-      // ]);
-
-      // await transactions.commit();
+      await this.createOnDatabase(
+        retirementPlanningRgps.id,
+        updatedRetirementPlanningRgps,
+        periods,
+      );
     }
-
-    // const cnis = await this.analysisProcessorGateway.parseCnisDocument(
-    //   dto.cnisDocument.buffer,
-    // );
 
     return CreateRetirementPlanningRgpsCnisResponseDto.build({
       retirementPlanningRgpsId: retirementPlanningRgps.id,
     });
+  }
+
+  private async createOnDatabase(
+    id: RetirementPlanningRgpsId,
+    retirementPlanningRgps: RetirementPlanningRgpsEntity,
+    retirementPlanningRgpsPeriod?: RetirementPlanningRgpsPeriodEntity[],
+  ): Promise<void> {
+    const retirementPlanningRgpsPeriodTransaction =
+      retirementPlanningRgpsPeriod?.map((value) => {
+        return this.retirementPlanningRgpsPeriodCommandRepositoryGateway.createRetirementPlanningRgpsPeriod(
+          value,
+        );
+      }) ?? [];
+    const retirementPlanningRgpsTransaction =
+      this.retirementPlanningRgpsCommandRepositoryGateway.updateRetirementPlanningRgps(
+        id,
+        retirementPlanningRgps,
+      );
+
+    const transactions = await this.baseTransactionRepositoryGateway.execute([
+      ...retirementPlanningRgpsPeriodTransaction,
+      retirementPlanningRgpsTransaction,
+    ]);
+
+    await transactions.commit();
   }
 }
