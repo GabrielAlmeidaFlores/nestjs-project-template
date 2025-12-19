3e602b164a77d65594668fb76561df74f594b5f4
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-12-19 06:06:31 -0300

    feat(organization-credit): implement ConsumeOrganizationCredit use case and related gateway; add InsufficientCreditsError class

--- src/module/customer/organization-credit/organization-credit.module.ts
+++ src/module/customer/organization-credit/organization-credit.module.ts
@@ -2,14 +2,24 @@ import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '@infra/database/database.module';
 import { OrganizationCreditController } from '@module/customer/organization-credit/organization-credit.controller';
+import { ConsumeOrganizationCreditUseCase } from '@module/customer/organization-credit/use-case/consume-organization-credit.use-case';
 import { GetOrganizationAvailableCreditsUseCase } from '@module/customer/organization-credit/use-case/get-organization-available-credits.use-case';
+import { ConsumeOrganizationCreditUseCaseGateway } from '@module/customer/organization-credit/use-case-gateway/consume-organization-credit.use-case-gateway';
 import { AuthModule } from '@shared/api/gateway/guard/auth/auth.module';
 import { OrganizationSessionModule } from '@shared/api/gateway/guard/organization-session/organization-session.module';
 
 @Module({
   imports: [AuthModule, DatabaseModule, OrganizationSessionModule],
   controllers: [OrganizationCreditController],
-  providers: [GetOrganizationAvailableCreditsUseCase],
+  providers: [
+    GetOrganizationAvailableCreditsUseCase,
+    ConsumeOrganizationCreditUseCase,
+    {
+      provide: ConsumeOrganizationCreditUseCaseGateway,
+      useClass: ConsumeOrganizationCreditUseCase,
+    },
+  ],
+  exports: [ConsumeOrganizationCreditUseCaseGateway],
 })
 export class OrganizationCreditModule {
   protected readonly _type = OrganizationCreditModule.name;
