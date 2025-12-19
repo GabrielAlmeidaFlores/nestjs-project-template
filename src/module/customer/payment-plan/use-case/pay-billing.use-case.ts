11a6c478d246008ba389771f510a764c7920dcbf
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-12-19 17:17:07 -0300

    feat(payment): add transaction handling for bank payment updates in PayBillingUseCase

--- src/module/customer/payment-plan/use-case/pay-billing.use-case.ts
+++ src/module/customer/payment-plan/use-case/pay-billing.use-case.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 
+import { BaseTransactionRepositoryGateway } from '@core/domain/repository/base/transaction/base.transaction.repository.gateway';
 import { CreditCardHolderInfoInputModel } from '@infra/payment-gateway/model/input/credit-card-holder.input.model';
 import { CreditCardInfoInputModel } from '@infra/payment-gateway/model/input/credit-card-info.input.model';
 import { PayBillingInputModel } from '@infra/payment-gateway/model/input/pay-billing.input.model';
@@ -9,7 +10,10 @@ import { PayBillingRequestDto } from '@module/customer/payment-plan/dto/request/
 import { PayBillingResponseDto } from '@module/customer/payment-plan/dto/response/pay-billing.response.dto';
 import { BankPaymentNotFoundError } from '@module/customer/payment-plan/error/bank-payment-not-found.error';
 import { DeleteOrganizationPaymentPlanUseCase } from '@module/customer/payment-plan/use-case/delete-organization-payment-plan.use-case';
+import { BankPaymentCommandRepositoryGateway } from '@module/generic/bank/domain/repository/bank-payment/command/bank-payment.command.repository.gateway';
 import { BankPaymentQueryRepositoryGateway } from '@module/generic/bank/domain/repository/bank-payment/query/bank-payment.query.repository.gateway';
+import { BankPaymentEntity } from '@module/generic/bank/domain/schema/entity/bank-payment/bank-payment.entity';
+import { PaymentStatusEnum } from '@module/generic/bank/domain/schema/entity/bank-payment/enum/payment-status.enum';
 import { BankPaymentId } from '@module/generic/bank/domain/schema/entity/bank-payment/value-object/bank-payment-id/bank-payment-id.value-object';
 import { OrganizationSessionDataModel } from '@shared/api/util/decorator/property/get-organization-session-data/model/generic/organization-session-data.model';
 
@@ -20,10 +24,14 @@ export class PayBillingUseCase {
   public constructor(
     @Inject(BankPaymentQueryRepositoryGateway)
     private readonly bankPaymentQueryRepository: BankPaymentQueryRepositoryGateway,
+    @Inject(BankPaymentCommandRepositoryGateway)
+    private readonly bankPaymentCommandRepository: BankPaymentCommandRepositoryGateway,
     @Inject(OrganizationPaymentPlanBankPaymentQueryRepositoryGateway)
     private readonly organizationPaymentPlanBankPaymentQueryRepository: OrganizationPaymentPlanBankPaymentQueryRepositoryGateway,
     @Inject(PaymentGateway)
     private readonly paymentGateway: PaymentGateway,
+    @Inject(BaseTransactionRepositoryGateway)
+    private readonly baseTransactionRepositoryGateway: BaseTransactionRepositoryGateway,
     private readonly deleteOrganizationPaymentPlanUseCase: DeleteOrganizationPaymentPlanUseCase,
   ) {}
 
@@ -61,6 +69,22 @@ export class PayBillingUseCase {
         creditCardHolderInfo,
       }),
     );
+
+    const updatedBankPayment = new BankPaymentEntity({
+      ...bankPayment,
+      status: PaymentStatusEnum.CONFIRMED,
+    });
+
+    const updateBankPaymentTransaction =
+      this.bankPaymentCommandRepository.updateBankPayment(
+        bankPayment.id,
+        updatedBankPayment,
+      );
+
+    const transaction = await this.baseTransactionRepositoryGateway.execute([
+      updateBankPaymentTransaction,
+    ]);
+    await transaction.commit();
 
     const organizationPaymentPlanBankPayment =
       await this.organizationPaymentPlanBankPaymentQueryRepository.findOneOrganizationPaymentPlanBankPaymentByBankPaymentId(
