062e4f13d1bbd69b883b6dbcc4c48c79c6d100b7
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-12-21 14:15:04 -0300

    fix(payment-billing): update PayBillingRequestDto to remove bankPaymentId and adjust controller and use case accordingly

--- src/module/customer/payment-plan/dto/request/pay-billing.request.dto.ts
+++ src/module/customer/payment-plan/dto/request/pay-billing.request.dto.ts
@@ -4,14 +4,10 @@ import {
 } from '@module/customer/payment-plan/dto/request/credit-card.request.dto';
 import { RequestDto } from '@shared/api/util/decorator/class/dto-specification/request-dto.decorator';
 import { RequestDtoObjectProperty } from '@shared/api/util/decorator/property/dto-property/request/request-dto-object-property/request-dto-object-property.decorator';
-import { RequestDtoStringProperty } from '@shared/api/util/decorator/property/dto-property/request/request-dto-string-property/request-dto-string-property.decorator';
 import { BaseBuildableDtoObject } from '@shared/api/util/object/base-buildable-dto.object';
 
 @RequestDto()
 export class PayBillingRequestDto extends BaseBuildableDtoObject {
-  @RequestDtoStringProperty({ description: 'ID do pagamento bancário' })
-  public bankPaymentId: string;
-
   @RequestDtoObjectProperty(() => CreditCardInfoRequestDto)
   public creditCardInfo: CreditCardInfoRequestDto;
 
