149fd11b354045689b00e5477e6bf38c62a1f157
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-12-22 19:51:34 -0300

    feat: adds chat ID in response about chat history

--- src/module/ai/infra/chat/dto/response/get-conversation.response.dto.ts
+++ src/module/ai/infra/chat/dto/response/get-conversation.response.dto.ts
@@ -1,12 +1,17 @@
 import { ConversationStatusTypeEnum } from '@module/ai/infra/chat/domain/schema/entity/conversation/enum/conversation-status-type-enum';
+import { ConversationId } from '@module/ai/infra/chat/domain/schema/entity/conversation/value-object/conversation-id/conversation-id.value-object';
 import { ResponseDto } from '@shared/api/util/decorator/class/dto-specification/response-dto.decorator';
 import { ResponseDtoDateProperty } from '@shared/api/util/decorator/property/dto-property/response/response-dto-date-property/response-dto-date-property.decorator';
 import { ResponseDtoEnumProperty } from '@shared/api/util/decorator/property/dto-property/response/response-dto-enum-property/response-dto-enum-property.decorator';
 import { ResponseDtoStringProperty } from '@shared/api/util/decorator/property/dto-property/response/response-dto-string-property/response-dto-string-property.decorator';
+import { ResponseDtoValueObjectProperty } from '@shared/api/util/decorator/property/dto-property/response/response-dto-value-object-property/response-dto-value-object-property.decorator';
 import { BaseBuildableDtoObject } from '@shared/api/util/object/base-buildable-dto.object';
 
 @ResponseDto()
 export class GetConversationResponseDto extends BaseBuildableDtoObject {
+  @ResponseDtoValueObjectProperty(ConversationId)
+  public readonly id: ConversationId;
+
   @ResponseDtoStringProperty({ required: false })
   public readonly assistantType?: string | null;
 
