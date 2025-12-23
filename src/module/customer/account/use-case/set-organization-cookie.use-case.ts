5b4a2eafe8a6e29011b1aa9d49685dbe8de9ea46
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-12-23 15:55:48 -0300

    feat(customer): implement SetOrganizationCookieUseCase and refactor SetOrganizationForCustomerUseCase to utilize it

---
+++ src/module/customer/account/use-case/set-organization-cookie.use-case.ts
@@ -0,0 +1,37 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { FastifyReply } from 'fastify';
+
+import { OrganizationSessionGateway } from '@module/customer/account/lib/organization-session/organization-session.gateway';
+import { SetOrganizationCookieUseCaseGateway } from '@module/customer/account/use-case-gateway/set-organization-cookie.use-case-gateway';
+import { ApiCookieEnum } from '@shared/api/enum/api-cookie.enum';
+
+import type { OrganizationId } from '@module/customer/account/domain/schema/entity/organization/value-object/organization-id/organization-id.value-object';
+
+@Injectable()
+export class SetOrganizationCookieUseCase implements SetOrganizationCookieUseCaseGateway {
+  protected readonly _type = SetOrganizationCookieUseCase.name;
+  private readonly sevenDaysInSeconds: number;
+
+  public constructor(
+    @Inject(OrganizationSessionGateway)
+    private readonly organizationSessionGateway: OrganizationSessionGateway,
+  ) {
+    this.sevenDaysInSeconds = 604800;
+  }
+
+  public execute(
+    reply: FastifyReply,
+    organizationId: OrganizationId,
+    owner: boolean,
+  ): void {
+    const jwtOrganizationSession =
+      this.organizationSessionGateway.createSession(organizationId, owner);
+    reply.setCookie(ApiCookieEnum.ORGANIZATION, jwtOrganizationSession, {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax',
+      path: '/',
+      maxAge: this.sevenDaysInSeconds,
+    });
+  }
+}
