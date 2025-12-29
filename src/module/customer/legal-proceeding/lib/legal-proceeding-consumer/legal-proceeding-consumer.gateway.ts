97af4d76fd4956e4950868b723a9cc84e8b9d8ef
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-12-29 19:10:35 -0300

    feat(legal-proceeding): implement LegalProceedingConsumerGateway in ComunicacaoPjeService and refactor module providers

--- src/module/customer/legal-proceeding/lib/legal-proceeding-consumer/legal-proceeding-consumer.gateway.ts
+++ src/module/customer/legal-proceeding/lib/legal-proceeding-consumer/legal-proceeding-consumer.gateway.ts
@@ -1,22 +1,7 @@
 import { Injectable } from '@nestjs/common';
-
-import { ComunicacaoPjeService } from '@module/customer/legal-proceeding/lib/legal-proceeding-consumer/comunicacao-pje/comunicacao-pje.service';
-
 @Injectable()
-export class LegalProceedingConsumerGateway {
-  protected readonly _type = LegalProceedingConsumerGateway.name;
-
-  public constructor(
-    private readonly comunicacaoPjeService: ComunicacaoPjeService,
-  ) {}
-
-  public async consumeByProcessNumber(
+export abstract class LegalProceedingConsumerGateway {
+  public abstract consumeByProcessNumber(
     legalProceedingNumber: string,
-  ): Promise<object> {
-    const response = await this.comunicacaoPjeService.getLegalProceeding(
-      legalProceedingNumber,
-    );
-
-    return response;
-  }
+  ): Promise<object>;
 }
