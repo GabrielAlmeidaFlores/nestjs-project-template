bf518dec1a62f7581108a2815e0b116e9c4f8581
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2025-12-29 19:02:28 -0300

    feat(legal-proceeding): refactor legal proceeding detail models and remove unused DTOs

--- src/module/customer/legal-proceeding/use-case/list-legal-proceeding-detail.use-case.ts
+++ src/module/customer/legal-proceeding/use-case/list-legal-proceeding-detail.use-case.ts
@@ -6,7 +6,7 @@ import { LegalProceedingDetailQueryRepositoryGateway } from '@module/customer/le
 import { ListLegalProceedingDetailQueryParam } from '@module/customer/legal-proceeding/domain/repository/legal-proceeding-detail/query/param/list-legal-proceeding-detail.query.param';
 import { GetLegalProceedingDetailLaywerWithRelationsResponseDto } from '@module/customer/legal-proceeding/dto/response/get-legal-proceeding-detail-laywer-with-relations.response.dto';
 import { ListLegalProceedingDetailLaywerResponseDto } from '@module/customer/legal-proceeding/dto/response/list-legal-proceeding-detail-laywer.response.dto';
-import { DetailParsedType } from '@module/customer/legal-proceeding/type/legal-proceeding-detail.type';
+import { LegalProceedingDetailModel } from '@module/customer/legal-proceeding/lib/legal-proceeding-consumer/comunicacao-pje/model/generic/legal-proceeding-detail.model';
 
 import type { ListLegalProceedingDetailRequestDto } from '@module/customer/legal-proceeding/dto/request/list-legal-proceeding-detail.request.dto';
 import type { OrganizationSessionDataModel } from '@shared/api/util/decorator/property/get-organization-session-data/model/generic/organization-session-data.model';
@@ -47,7 +47,9 @@ export class ListLegalProceedingDetailUseCase {
 
     const resource: GetLegalProceedingDetailLaywerWithRelationsResponseDto[] =
       legalProceedingDetailList.resource.map((item) => {
-        const detailParsed = JSON.parse(item.detail) as DetailParsedType;
+        const detailParsed = JSON.parse(
+          item.detail,
+        ) as LegalProceedingDetailModel;
 
         const items = detailParsed.data?.items ?? [];
         const lastItem = items.length > 0 ? items[items.length - 1] : null;
