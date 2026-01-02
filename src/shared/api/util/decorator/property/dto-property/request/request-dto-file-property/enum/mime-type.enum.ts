97fcc1f320a89764afaf33b92c2dc368c517021c
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-01-02 17:41:16 -0300

    fix: adds more types of audio submissions for transcription

--- src/shared/api/util/decorator/property/dto-property/request/request-dto-file-property/enum/mime-type.enum.ts
+++ src/shared/api/util/decorator/property/dto-property/request/request-dto-file-property/enum/mime-type.enum.ts
@@ -3,5 +3,13 @@ export enum MimeTypeEnum {
   IMAGE_PNG = 'image/png',
   APPLICATION_PDF = 'application/pdf',
   AUDIO_MPEG = 'audio/mpeg',
+  AUDIO_WEBM = 'audio/webm',
+  AUDIO_WAV = 'audio/wav',
+  AUDIO_X_WAV = 'audio/x-wav',
+  AUDIO_WAVE = 'audio/wave',
+  AUDIO_VND_WAVE = 'audio/vnd.wave',
+  APPLICATION_OCTET_STREAM = 'application/octet-stream',
+  VIDEO_WEBM = 'video/webm',
+  AUDIO_OGG = 'audio/ogg',
   WORD = 'application/msword',
 }
