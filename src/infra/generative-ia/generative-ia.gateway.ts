fca8ba3f1b1270352ac01912860e3d1b99dd7f53
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-01-03 10:13:33 -0300

    feat: implement generateHighQualityResponseFromPromptAndFilesWithContract method in Gemini and LoremIpsum services

--- src/infra/generative-ia/generative-ia.gateway.ts
+++ src/infra/generative-ia/generative-ia.gateway.ts
@@ -1,3 +1,4 @@
+import type { SchemaUnion } from '@google/genai';
 import type { GenerateResponseInputModel } from '@infra/generative-ia/implementation/model/input/generate-response.input.model';
 
 export abstract class GenerativeIaGateway {
@@ -7,5 +8,10 @@ export abstract class GenerativeIaGateway {
 
   public abstract generateFlashResponseFromPromptAndFiles(
     props: GenerateResponseInputModel,
+  ): Promise<string | null>;
+
+  public abstract generateHighQualityResponseFromPromptAndFilesWithContract(
+    props: GenerateResponseInputModel,
+    responseSchema?: SchemaUnion,
   ): Promise<string | null>;
 }
--- src/infra/generative-ia/implementation/lorem-ipsum/lorem-ipsum.service.ts
+++ src/infra/generative-ia/implementation/lorem-ipsum/lorem-ipsum.service.ts
@@ -46,4 +46,25 @@ For example, you might want to add sections for:
       );
     });
   }
+
+  public async generateHighQualityResponseFromPromptAndFilesWithContract(
+    _: GenerateResponseInputModel,
+  ): Promise<string | null> {
+    return new Promise((resolve) => {
+      resolve(
+        `
+# Project Title
+
+### A brief subtitle that describes the project's main purpose.
+
+This is the main body of the README. This section should provide a more detailed overview of the project. Explain the problem it solves, the main features, and the technologies used. You can also include information about the project's status, its goals, and any key differentiators.
+
+For example, you might want to add sections for:
+* **Installation:** How to get the project set up and running.
+* **Usage:** How to use the project after installation.
+* **Contributing:** Guidelines for how others can contribute.
+* **License:** Information about the project's license.`,
+      );
+    });
+  }
 }
