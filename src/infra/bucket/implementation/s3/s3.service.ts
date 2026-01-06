93e89bd9bcd9d730980eb7ea5d43be714c12766d
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-01-06 14:32:43 -0300

    fix: correct type casting for byte array in getBuffer method and reorder imports in CreateLegalPleadingDocumentAnalysisUseCase

--- src/infra/bucket/implementation/s3/s3.service.ts
+++ src/infra/bucket/implementation/s3/s3.service.ts
@@ -108,7 +108,7 @@ export class S3Service implements BucketGateway {
     const response = await this.s3Client.send(command);
 
     const byteArray = await response.Body?.transformToByteArray();
-    return Buffer.from(byteArray as Uint8Array<ArrayBufferLike>);
+    return Buffer.from(byteArray as Uint8Array);
   }
 
   public async delete(fileName: string): Promise<void> {
