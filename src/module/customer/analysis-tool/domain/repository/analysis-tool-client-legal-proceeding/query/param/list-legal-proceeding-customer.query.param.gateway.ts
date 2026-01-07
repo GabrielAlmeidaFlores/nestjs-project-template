0d10aab20493710e3e815f99385b737686a53b5b
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-01-07 18:30:05 -0300

    feat: Enhance legal proceeding filtering and response DTOs with additional parameters

--- src/module/customer/analysis-tool/domain/repository/analysis-tool-client-legal-proceeding/query/param/list-legal-proceeding-customer.query.param.gateway.ts
+++ src/module/customer/analysis-tool/domain/repository/analysis-tool-client-legal-proceeding/query/param/list-legal-proceeding-customer.query.param.gateway.ts
@@ -3,6 +3,14 @@ import { ListDataInputModel } from '@core/domain/repository/base/query/model/inp
 export class ListLegalProceedingCustomerQueryParamGateway extends ListDataInputModel {
   public readonly analysisToolClientId: string | null;
 
+  public readonly status: string | null;
+
+  public readonly legalProceedingNumber: string | null;
+
+  public readonly startDate: Date | null;
+
+  public readonly endDate: Date | null;
+
   protected override readonly _type =
     ListLegalProceedingCustomerQueryParamGateway.name;
   public constructor(
@@ -11,5 +19,9 @@ export class ListLegalProceedingCustomerQueryParamGateway extends ListDataInputM
     super(props);
 
     this.analysisToolClientId = props.analysisToolClientId ?? null;
+    this.status = props.status ?? null;
+    this.legalProceedingNumber = props.legalProceedingNumber ?? '';
+    this.startDate = props.startDate ?? null;
+    this.endDate = props.endDate ?? null;
   }
 }
