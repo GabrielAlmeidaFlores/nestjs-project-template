f846d832ce220458f06c25dc85669cda596bc121
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-01-12 11:25:25 -0300

    refactor(cnis-fast-analysis): clean up module imports and exports

--- src/module/customer/analysis-tool/module/cnis-fast-analysis/cnis-fast-analysis.module.ts
+++ src/module/customer/analysis-tool/module/cnis-fast-analysis/cnis-fast-analysis.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '@infra/database/database.module';
-import { GenerativeIaModule } from '@infra/generative-ia/generative-ia.module';
 import { CnisAnalyzerModule } from '@lib/cnis-analyzer/cnis-analyzer.module';
-import { PaymentPlanModule } from '@module/admin/payment-plan/payment-plan.module';
-import { FileProcessorModule } from '@module/customer/account/lib/file-processor/file-processor.module';
-import { OrganizationSessionModule } from '@module/customer/account/lib/organization-session/organization-session.module';
 import { AnalysisProcessorModule } from '@module/customer/analysis-tool/lib/analysis-processor/analysis-processor.module';
 import { ExportDocumentModule } from '@module/customer/analysis-tool/lib/export-document/export-document.module';
-import { RemunerationCalculatorModule } from '@module/customer/analysis-tool/lib/remuneration-calculator/remuneration-calculator.module';
+import { FileProcessorModule } from '@module/customer/analysis-tool/lib/file-processor/file-processor.module';
 import { CnisFastAnalysisController } from '@module/customer/analysis-tool/module/cnis-fast-analysis/cnis-fast-analysis.controller';
 import { CreateCnisFastAnalysisResultUseCase } from '@module/customer/analysis-tool/module/cnis-fast-analysis/use-case/create-cnis-fast-analysis-result.use-case';
 import { CreateCnisFastAnalysisUseCase } from '@module/customer/analysis-tool/module/cnis-fast-analysis/use-case/create-cnis-fast-analysis.use-case';
@@ -18,7 +14,9 @@ import { DownloadCnisSimplifiedAnalysisUseCase } from '@module/customer/analysis
 import { GetCnisFastAnalysisUseCase } from '@module/customer/analysis-tool/module/cnis-fast-analysis/use-case/get-cnis-fast-analysis.use-case';
 import { UpdateCnisFastAnalysisUseCase } from '@module/customer/analysis-tool/module/cnis-fast-analysis/use-case/update-cnis-fast-analysis.use-case';
 import { OrganizationCreditModule } from '@module/customer/organization-credit/organization-credit.module';
+import { PaymentPlanModule } from '@module/customer/payment-plan/payment-plan.module';
 import { AuthModule } from '@shared/api/gateway/guard/auth/auth.module';
+import { OrganizationSessionModule } from '@shared/api/gateway/guard/organization-session/organization-session.module';
 
 @Module({
   imports: [
@@ -30,9 +28,7 @@ import { AuthModule } from '@shared/api/gateway/guard/auth/auth.module';
     FileProcessorModule,
     AnalysisProcessorModule,
     ExportDocumentModule,
-    RemunerationCalculatorModule,
     CnisAnalyzerModule,
-    GenerativeIaModule,
   ],
   controllers: [CnisFastAnalysisController],
   providers: [
@@ -44,15 +40,7 @@ import { AuthModule } from '@shared/api/gateway/guard/auth/auth.module';
     DownloadCnisSimplifiedAnalysisUseCase,
     UpdateCnisFastAnalysisUseCase,
   ],
-  exports: [
-    CreateCnisFastAnalysisUseCase,
-    CreateCnisFastAnalysisResultUseCase,
-    GetCnisFastAnalysisUseCase,
-    DeleteCnisFastAnalysisUseCase,
-    DownloadCnisCompleteAnalysisUseCase,
-    DownloadCnisSimplifiedAnalysisUseCase,
-    UpdateCnisFastAnalysisUseCase,
-  ],
+  exports: [DeleteCnisFastAnalysisUseCase],
 })
 export class CnisFastAnalysisModule {
   protected readonly _type = CnisFastAnalysisModule.name;
