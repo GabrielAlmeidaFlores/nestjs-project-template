f925e9715c4b4ee1612310fb2ebb7bc5e2b599cc
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-01-16 16:27:19 -0300

    feat: enhance legal pleading document analysis use case with payment plan resource handling

--- src/module/customer/analysis-tool/use-case/create-legal-pleading-document-analysis.use-case.ts
+++ src/module/customer/analysis-tool/use-case/create-legal-pleading-document-analysis.use-case.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 
 import { BaseTransactionRepositoryGateway } from '@core/domain/repository/base/transaction/base.transaction.repository.gateway';
 import { TransactionType } from '@core/domain/repository/base/transaction/type/transaction.type';
+import { PaymentPlanPaidResourceNotFoundError } from '@module/admin/payment-plan/error/payment-plan-paid-resource-not-found.error';
 import { OrganizationMemberQueryRepositoryGateway } from '@module/customer/account/domain/repository/organization-member/query/organization-member.query.repository.gateway';
 import { LegalPleadingQueryRepositoryGateway } from '@module/customer/analysis-tool/domain/repository/legal-pleading/query/legal-pleading.query.repository.gateway';
 import { LegalPleadingDocumentCommandRepositoryGateway } from '@module/customer/analysis-tool/domain/repository/legal-pleading-document/command/legal-pleading-document.repository.gateway';
@@ -25,6 +26,7 @@ import { AnalysisProcessorGateway } from '@module/customer/analysis-tool/lib/ana
 import { ExportDocumentGateway } from '@module/customer/analysis-tool/lib/export-document/export-document.gateway';
 import { FileProcessorGateway } from '@module/customer/analysis-tool/lib/file-processor/file-processor.gateway';
 import { ConsumeOrganizationCreditUseCaseGateway } from '@module/customer/organization-credit/use-case-gateway/consume-organization-credit.use-case-gateway';
+import { PaymentPlanPaidResourceQueryRepositoryGateway } from '@module/customer/payment-plan/domain/repository/payment-plan-paid-resource/query/payment-plan-paid-resource.query.repository.gateway';
 import { PaymentPlanPaidResourceTypeEnum } from '@module/customer/payment-plan/domain/schema/entity/payment-plan-paid-resource/enum/payment-plan-paid-resource-type.enum';
 import { GetPaymentPlanPaidResourcePromptUseCaseGateway } from '@module/customer/payment-plan/use-case-gateway/get-payment-plan-paid-resource-prompt.use-case-gateway';
 import { OrganizationSessionDataModel } from '@shared/api/util/decorator/property/get-organization-session-data/model/generic/organization-session-data.model';
@@ -55,6 +57,8 @@ export class CreateLegalPleadingDocumentAnalysisUseCase {
     private readonly getPaymentPlanPaidResourcePromptUseCase: GetPaymentPlanPaidResourcePromptUseCaseGateway,
     @Inject(ConsumeOrganizationCreditUseCaseGateway)
     private readonly consumeOrganizationCreditUseCase: ConsumeOrganizationCreditUseCaseGateway,
+    @Inject(PaymentPlanPaidResourceQueryRepositoryGateway)
+    private readonly paymentPlanPaidResourceQueryRepositoryGateway: PaymentPlanPaidResourceQueryRepositoryGateway,
     @Inject(ExportDocumentGateway)
     private readonly exportDocumentGateway: ExportDocumentGateway,
   ) {}
@@ -74,17 +78,14 @@ export class CreateLegalPleadingDocumentAnalysisUseCase {
       throw new OrganizationMemberNotFoundError();
     }
 
-    const promptResponse =
-      await this.getPaymentPlanPaidResourcePromptUseCase.execute(
+    const paymentPlanPaidResource =
+      await this.paymentPlanPaidResourceQueryRepositoryGateway.findOnePaymentPlanPaidResourceByResourceType(
         PaymentPlanPaidResourceTypeEnum.LEGAL_PLEADING_QUICK_DOCUMENT_ANALYSIS,
       );
 
-    const consumeCreditTransaction =
-      await this.consumeOrganizationCreditUseCase.execute(
-        organizationSessionData.organizationId,
-        PaymentPlanPaidResourceTypeEnum.LEGAL_PLEADING_QUICK_DOCUMENT_ANALYSIS,
-        organizationMember.id,
-      );
+    if (paymentPlanPaidResource === null) {
+      throw new PaymentPlanPaidResourceNotFoundError();
+    }
 
     const legalPleadingQueryResult =
       await this.legalPleadingQueryRepositoryGateway.findOneByLegalPleadingIdAndOrganizationIdAndAuthIdentityIdOrFail(
@@ -92,6 +93,18 @@ export class CreateLegalPleadingDocumentAnalysisUseCase {
         organizationSessionData.organizationId,
         sessionData.authIdentityId,
         LegalPleadingNotFoundError,
+      );
+
+    const consumeCreditTransaction =
+      await this.consumeOrganizationCreditUseCase.execute(
+        organizationSessionData.organizationId,
+        PaymentPlanPaidResourceTypeEnum.LEGAL_PLEADING_QUICK_DOCUMENT_ANALYSIS,
+        organizationMember.id,
+        {
+          explicitCreditCost:
+            paymentPlanPaidResource.creditCost *
+            legalPleadingQueryResult.legalPleadingDocument.length,
+        },
       );
 
     const analysisToolClient = new AnalysisToolClientEntity({
@@ -143,6 +156,11 @@ export class CreateLegalPleadingDocumentAnalysisUseCase {
     const responseData: CreateLegalPleadingDocumentTypeAnalysisResponseDto[] =
       [];
 
+    const legalPleadingQuickDocumentAnalysisPrompt =
+      await this.getPaymentPlanPaidResourcePromptUseCase.execute(
+        PaymentPlanPaidResourceTypeEnum.LEGAL_PLEADING_QUICK_DOCUMENT_ANALYSIS,
+      );
+
     await Promise.all(
       Object.keys(documentGroup).map(async (key) => {
         const documentType = key as LegalPleadingDocumentTypeEnum;
@@ -150,7 +168,7 @@ export class CreateLegalPleadingDocumentAnalysisUseCase {
 
         const documentAnalysis =
           await this.analysisProcessorGateway.getLegalPleadingQuickDocumentAnalysis(
-            promptResponse.prompt,
+            legalPleadingQuickDocumentAnalysisPrompt.prompt,
             documents,
           );
 
