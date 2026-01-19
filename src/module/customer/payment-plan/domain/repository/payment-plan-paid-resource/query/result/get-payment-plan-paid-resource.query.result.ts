5a391c25af8ca39c3fe3ad0022016f693f90584b
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-01-19 10:57:11 -0300

    feat: add title field to PaymentPlanPaidResource entity and related mappings

--- src/module/customer/payment-plan/domain/repository/payment-plan-paid-resource/query/result/get-payment-plan-paid-resource.query.result.ts
+++ src/module/customer/payment-plan/domain/repository/payment-plan-paid-resource/query/result/get-payment-plan-paid-resource.query.result.ts
@@ -7,6 +7,7 @@ export class GetPaymentPlanPaidResourceQueryResult extends BaseBuildableObject {
   public readonly id: PaymentPlanPaidResourceId;
   public readonly resource: PaymentPlanPaidResourceTypeEnum;
   public readonly creditCost: number;
+  public readonly title: string;
   public readonly description: string;
   public readonly createdAt: Date;
   public readonly updatedAt: Date;
