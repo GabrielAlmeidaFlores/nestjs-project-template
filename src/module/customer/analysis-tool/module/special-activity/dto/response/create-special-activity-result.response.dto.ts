438db7e82faa477b0428027960a46ce1ca961075
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-01-20 16:02:05 -0300

    feat: update special activity analysis response DTO and use case to reflect new analysis structure

--- src/module/customer/analysis-tool/module/special-activity/dto/response/create-special-activity-result.response.dto.ts
+++ src/module/customer/analysis-tool/module/special-activity/dto/response/create-special-activity-result.response.dto.ts
@@ -1,14 +1,9 @@
-import { SpecialActivityId } from '@module/customer/analysis-tool/domain/schema/entity/special-activity/value-object/special-activity-id.value-object';
 import { ResponseDto } from '@shared/api/util/decorator/class/dto-specification/response-dto.decorator';
 import { ResponseDtoObjectProperty } from '@shared/api/util/decorator/property/dto-property/response/response-dto-object-property/response-dto-object-property.decorator';
-import { ResponseDtoValueObjectProperty } from '@shared/api/util/decorator/property/dto-property/response/response-dto-value-object-property/response-dto-value-object-property.decorator';
 import { BaseBuildableDtoObject } from '@shared/api/util/object/base-buildable-dto.object';
 
 @ResponseDto()
 export class CreateSpecialActivityResultResponseDto extends BaseBuildableDtoObject {
-  @ResponseDtoValueObjectProperty(SpecialActivityId)
-  public specialActivityId: SpecialActivityId;
-
   @ResponseDtoObjectProperty(() => Object, { required: false })
   public specialActivityCompleteAnalysis?: object;
 
