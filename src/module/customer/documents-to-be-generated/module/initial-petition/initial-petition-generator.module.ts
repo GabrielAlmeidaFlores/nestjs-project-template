97bacd059f8d0f4431825ab15836fdb5ca958993
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-02-03 11:39:17 -0300

    feat: rename documentProcessorGateway to documentGeneratorProcessorGateway for consistency across modules

--- src/module/customer/documents-to-be-generated/module/initial-petition/initial-petition-generator.module.ts
+++ src/module/customer/documents-to-be-generated/module/initial-petition/initial-petition-generator.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '@infra/database/database.module';
-import { AnalysisProcessorModule } from '@module/customer/analysis-tool/lib/analysis-processor/analysis-processor.module';
 import { ExportDocumentModule } from '@module/customer/analysis-tool/lib/export-document/export-document.module';
+import { DocumentGeneratorProcessorModule } from '@module/customer/documents-to-be-generated/lib/document-generator-processor/document-generator-processor.module';
 import { InitialPetitionGeneratorController } from '@module/customer/documents-to-be-generated/module/initial-petition/initial-petition-generator.controller';
 import { CreateInitialPetitionGeneratorUseCase } from '@module/customer/documents-to-be-generated/module/initial-petition/use-case/create-initial-petition-generator.use-case';
 import { DownloadInitialPetitionGeneratorCompleteAnalysisUseCase } from '@module/customer/documents-to-be-generated/module/initial-petition/use-case/download-initial-petition-generator-complete-analysis.use-case';
@@ -12,7 +12,7 @@ import { PaymentPlanModule } from '@module/customer/payment-plan/payment-plan.mo
 @Module({
   imports: [
     DatabaseModule,
-    AnalysisProcessorModule,
+    DocumentGeneratorProcessorModule,
     PaymentPlanModule,
     ExportDocumentModule,
   ],
