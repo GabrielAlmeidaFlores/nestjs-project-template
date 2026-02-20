611e7fb039412d9ec8b8f9946e01f76ac982b3f5
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-02-20 14:12:35 -0300

    🐛 fix(download-insurance-quality-analysis-complete-analysis): update filename format for downloaded insurance quality analysis

--- src/module/customer/analysis-tool/module/insurance-quality-analysis/use-case/download-insurance-quality-analysis-complete-analysis.use-case.ts
+++ src/module/customer/analysis-tool/module/insurance-quality-analysis/use-case/download-insurance-quality-analysis-complete-analysis.use-case.ts
@@ -246,7 +246,7 @@ export class DownloadInsuranceQualityAnalysisCompleteAnalysisUseCase {
     return await this.exportDocumentGateway.downloadFileAsStreamable(
       analysisSummary,
       format,
-      `insurance-quality-analysis-${insuranceQualityAnalysisId.toString()}`,
+      `analise-completa-de-qualidade-do-segurado-${insuranceQualityAnalysisId.toString()}`,
     );
   }
 
