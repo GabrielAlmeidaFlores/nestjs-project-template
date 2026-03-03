92e2147a8706be82e00adb42f84f591bf4919458
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-03-03 09:10:48 -0300

    feat(disability-retirement-planning): add download use cases for complete and simplified analysis, including error handling

--- src/module/customer/analysis-tool/module/disability-retirement-planning/disability-retirement-planning.module.ts
+++ src/module/customer/analysis-tool/module/disability-retirement-planning/disability-retirement-planning.module.ts
@@ -4,16 +4,19 @@ import { DatabaseModule } from '@infra/database/database.module';
 import { AnalysisProcessorModule } from '@module/customer/analysis-tool/lib/analysis-processor/analysis-processor.module';
 import { FileProcessorModule } from '@module/customer/analysis-tool/lib/file-processor/file-processor.module';
 import { DisabilityRetirementPlanningController } from '@module/customer/analysis-tool/module/disability-retirement-planning/disability-retirement-planning.controller';
-import { CreateDisabilityRetirementPlanningPeriodUseCase } from '@module/customer/analysis-tool/module/disability-retirement-planning/use-case/create-disability-retirement-planning-period.use-case';
 import { CreateDisabilityRetirementPlanningRemunerationUseCase } from '@module/customer/analysis-tool/module/disability-retirement-planning/use-case/create-disability-retirement-planning-remuneration.use-case';
+import { CreateDisabilityRetirementPlanningPeriodUseCase } from '@module/customer/analysis-tool/module/disability-retirement-planning/use-case/create-disability-retirement-planning-period.use-case';
 import { CreateDisabilityRetirementPlanningResultUseCase } from '@module/customer/analysis-tool/module/disability-retirement-planning/use-case/create-disability-retirement-planning-result.use-case';
 import { CreateDisabilityRetirementPlanningUseCase } from '@module/customer/analysis-tool/module/disability-retirement-planning/use-case/create-disability-retirement-planning.use-case';
 import { DeleteDisabilityRetirementPlanningUseCase } from '@module/customer/analysis-tool/module/disability-retirement-planning/use-case/delete-disability-retirement-planning.use-case';
-import { GetDisabilityRetirementPlanningRemunerationCalculationUseCase } from '@module/customer/analysis-tool/module/disability-retirement-planning/use-case/get-disability-retirement-planning-remuneration-calculation.use-case';
 import { GetDisabilityRetirementPlanningUseCase } from '@module/customer/analysis-tool/module/disability-retirement-planning/use-case/get-disability-retirement-planning.use-case';
+import { GetDisabilityRetirementPlanningRemunerationCalculationUseCase } from '@module/customer/analysis-tool/module/disability-retirement-planning/use-case/get-disability-retirement-planning-remuneration-calculation.use-case';
+import { DownloadDisabilityRetirementPlanningCompleteAnalysisUseCase } from '@module/customer/analysis-tool/module/disability-retirement-planning/use-case/download-disability-retirement-planning-complete-analysis.use-case';
+import { DownloadDisabilityRetirementPlanningSimplifiedAnalysisUseCase } from '@module/customer/analysis-tool/module/disability-retirement-planning/use-case/download-disability-retirement-planning-simplified-analysis.use-case';
+import { ExportDocumentModule } from '@module/customer/analysis-tool/lib/export-document/export-document.module';
 import { ListDisabilityRetirementPlanningRemunerationUseCase } from '@module/customer/analysis-tool/module/disability-retirement-planning/use-case/list-disability-retirement-planning-remuneration.use-case';
-import { UpdateDisabilityRetirementPlanningPeriodUseCase } from '@module/customer/analysis-tool/module/disability-retirement-planning/use-case/update-disability-retirement-planning-period.use-case';
 import { UpdateDisabilityRetirementPlanningRemunerationUseCase } from '@module/customer/analysis-tool/module/disability-retirement-planning/use-case/update-disability-retirement-planning-remuneration.use-case';
+import { UpdateDisabilityRetirementPlanningPeriodUseCase } from '@module/customer/analysis-tool/module/disability-retirement-planning/use-case/update-disability-retirement-planning-period.use-case';
 import { UpdateDisabilityRetirementPlanningUseCase } from '@module/customer/analysis-tool/module/disability-retirement-planning/use-case/update-disability-retirement-planning.use-case';
 import { OrganizationCreditModule } from '@module/customer/organization-credit/organization-credit.module';
 import { PaymentPlanModule } from '@module/customer/payment-plan/payment-plan.module';
@@ -26,6 +29,7 @@ import { OrganizationSessionModule } from '@shared/api/gateway/guard/organizatio
     DatabaseModule,
     AnalysisProcessorModule,
     FileProcessorModule,
+    ExportDocumentModule,
     OrganizationCreditModule,
     OrganizationSessionModule,
     PaymentPlanModule,
@@ -43,6 +47,8 @@ import { OrganizationSessionModule } from '@shared/api/gateway/guard/organizatio
     UpdateDisabilityRetirementPlanningUseCase,
     CreateDisabilityRetirementPlanningResultUseCase,
     DeleteDisabilityRetirementPlanningUseCase,
+    DownloadDisabilityRetirementPlanningCompleteAnalysisUseCase,
+    DownloadDisabilityRetirementPlanningSimplifiedAnalysisUseCase,
   ],
   exports: [DeleteDisabilityRetirementPlanningUseCase],
 })
