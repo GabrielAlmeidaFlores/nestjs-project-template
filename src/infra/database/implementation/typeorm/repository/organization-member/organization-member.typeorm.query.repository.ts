71710f67a50359297811204c704650cf6248aa59
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-03-25 12:03:19 -0300

    merge conflicts

--- src/infra/database/implementation/typeorm/repository/organization-member/organization-member.typeorm.query.repository.ts
+++ src/infra/database/implementation/typeorm/repository/organization-member/organization-member.typeorm.query.repository.ts
@@ -384,6 +384,33 @@ export class OrganizationMemberTypeormQueryRepository
     );
   }
 
+  public async findOneOrganizationMemberByCustomerIdWithDeleted(
+    customerId: CustomerId,
+  ): Promise<GetOrganizationMemberQueryResult | null> {
+    const data = await this.findOne({
+      where: {
+        customer: {
+          id: customerId.toString(),
+        },
+        owner: false,
+      },
+      withDeleted: true,
+      order: {
+        createdAt: 'DESC',
+      },
+    });
+
+    if (data === null) {
+      return null;
+    }
+
+    return this.mapperGateway.map(
+      data,
+      OrganizationMemberTypeormEntity,
+      GetOrganizationMemberQueryResult,
+    );
+  }
+
   private async countRowsByCreatedByMemberId(
     entityClass:
       | typeof LegalPleadingTypeormEntity
@@ -410,31 +437,5 @@ export class OrganizationMemberTypeormQueryRepository
     );
 
     return new Map(pairs);
-  }
-  public async findOneOrganizationMemberByCustomerIdWithDeleted(
-    customerId: CustomerId,
-  ): Promise<GetOrganizationMemberQueryResult | null> {
-    const data = await this.findOne({
-      where: {
-        customer: {
-          id: customerId.toString(),
-        },
-        owner: false,
-      },
-      withDeleted: true,
-      order: {
-        createdAt: 'DESC',
-      },
-    });
-
-    if (data === null) {
-      return null;
-    }
-
-    return this.mapperGateway.map(
-      data,
-      OrganizationMemberTypeormEntity,
-      GetOrganizationMemberQueryResult,
-    );
   }
 }
