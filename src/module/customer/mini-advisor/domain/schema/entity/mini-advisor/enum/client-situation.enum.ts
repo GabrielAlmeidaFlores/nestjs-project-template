33c8fdfeb2eeb1a93fffef48fa4bc62442ce67a0
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-04-01 15:03:09 -0300

    feat(mini-advisor): add 'IS_DEPENDENT_OF_DECEASED_PERSON' to ClientSituationEnum

--- src/module/customer/mini-advisor/domain/schema/entity/mini-advisor/enum/client-situation.enum.ts
+++ src/module/customer/mini-advisor/domain/schema/entity/mini-advisor/enum/client-situation.enum.ts
@@ -4,5 +4,6 @@ export enum ClientSituationEnum {
   NEVER_WORKED = 'never_worked',
   IS_SICK_OR_DISABLED = 'is_sick_or_disabled',
   IS_DISABLED_PERSON = 'is_disabled_person',
+  IS_DEPENDENT_OF_DECEASED_PERSON = 'is_dependent_of_deceased_person',
   OTHER = 'other',
 }
