3f9a471d4a23ccca0826d786dbb91ed0bf2a6a8c
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-04-17 08:39:10 -0300

    fix: disable synchronization in TypeORM configuration

--- src/infra/database/implementation/typeorm/typeorm.index.ts
+++ src/infra/database/implementation/typeorm/typeorm.index.ts
@@ -1291,7 +1291,7 @@ export class TypeormIndex {
     password: DatabaseApplicationVariable.DATABASE_PASSWORD,
     database: DatabaseApplicationVariable.DATABASE_NAME,
     entities: TypeormIndex.entities,
-    synchronize: true,
+    synchronize: false,
   };
 
   protected readonly _type = TypeormIndex.name;
