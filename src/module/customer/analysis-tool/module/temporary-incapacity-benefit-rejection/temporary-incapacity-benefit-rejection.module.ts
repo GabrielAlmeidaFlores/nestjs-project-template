aa863c0a6634562c347816398d67b5b585afecd3
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-04-22 18:12:32 -0300

    fix: dependency injection

--- src/module/customer/analysis-tool/module/temporary-incapacity-benefit-rejection/temporary-incapacity-benefit-rejection.module.ts
+++ src/module/customer/analysis-tool/module/temporary-incapacity-benefit-rejection/temporary-incapacity-benefit-rejection.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '@infra/database/database.module';
+import { GenerativeIaModule } from '@infra/generative-ia/generative-ia.module';
+import { CnisAnalyzerModule } from '@lib/cnis-analyzer/cnis-analyzer.module';
+import { MarkdownConverterModule } from '@lib/markdown-converter/markdown-converter.module';
+import { AnalysisProcessorModule } from '@module/customer/analysis-tool/lib/analysis-processor/analysis-processor.module';
+import { ExportDocumentModule } from '@module/customer/analysis-tool/lib/export-document/export-document.module';
+import { FileProcessorModule } from '@module/customer/analysis-tool/lib/file-processor/file-processor.module';
 import { TemporaryIncapacityBenefitRejectionController } from '@module/customer/analysis-tool/module/temporary-incapacity-benefit-rejection/temporary-incapacity-benefit-rejection.controller';
 import { CreateTemporaryIncapacityBenefitRejectionCompleteAnalysisDownloadUseCase } from '@module/customer/analysis-tool/module/temporary-incapacity-benefit-rejection/use-case/create-temporary-incapacity-benefit-rejection-complete-analysis-download.use-case';
 import { CreateTemporaryIncapacityBenefitRejectionDisabilityAnalysisUseCase } from '@module/customer/analysis-tool/module/temporary-incapacity-benefit-rejection/use-case/create-temporary-incapacity-benefit-rejection-disability-analysis.use-case';
@@ -17,9 +23,25 @@ import { UpdateTemporaryIncapacityBenefitRejectionInsuredStatusUseCase } from '@
 import { UpdateTemporaryIncapacityBenefitRejectionWorkPeriodsUseCase } from '@module/customer/analysis-tool/module/temporary-incapacity-benefit-rejection/use-case/update-temporary-incapacity-benefit-rejection-work-periods.use-case';
 import { UpdateTemporaryIncapacityBenefitRejectionUseCase } from '@module/customer/analysis-tool/module/temporary-incapacity-benefit-rejection/use-case/update-temporary-incapacity-benefit-rejection.use-case';
 import { UploadTemporaryIncapacityBenefitRejectionDocumentsUseCase } from '@module/customer/analysis-tool/module/temporary-incapacity-benefit-rejection/use-case/upload-temporary-incapacity-benefit-rejection-documents.use-case';
+import { OrganizationCreditModule } from '@module/customer/organization-credit/organization-credit.module';
+import { PaymentPlanModule } from '@module/customer/payment-plan/payment-plan.module';
+import { AuthModule } from '@shared/api/gateway/guard/auth/auth.module';
+import { OrganizationSessionModule } from '@shared/api/gateway/guard/organization-session/organization-session.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    AuthModule,
+    DatabaseModule,
+    GenerativeIaModule,
+    AnalysisProcessorModule,
+    ExportDocumentModule,
+    OrganizationSessionModule,
+    OrganizationCreditModule,
+    PaymentPlanModule,
+    FileProcessorModule,
+    CnisAnalyzerModule,
+    MarkdownConverterModule,
+  ],
   controllers: [TemporaryIncapacityBenefitRejectionController],
   providers: [
     CreateTemporaryIncapacityBenefitRejectionUseCase,
