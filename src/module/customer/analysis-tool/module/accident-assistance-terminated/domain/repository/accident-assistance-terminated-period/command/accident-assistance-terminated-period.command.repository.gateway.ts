a7a3fe635ebc4d7abf2c9d3a039aa48b10ab117c
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-04-24 16:01:40 -0300

    fix: correct import statements in accident assistance terminated period files

--- src/module/customer/analysis-tool/module/accident-assistance-terminated/domain/repository/accident-assistance-terminated-period/command/accident-assistance-terminated-period.command.repository.gateway.ts
+++ src/module/customer/analysis-tool/module/accident-assistance-terminated/domain/repository/accident-assistance-terminated-period/command/accident-assistance-terminated-period.command.repository.gateway.ts
@@ -1,6 +1,6 @@
 import type { TransactionType } from '@core/domain/repository/base/transaction/type/transaction.type';
-import type { AccidentAssistanceTerminatedPeriodEntity } from '@module/customer/analysis-tool/module/accident-assistance-terminated/domain/schema/entity/accident-assistance-terminated-period/accident-assistance-terminated-period.entity';
 import type { AccidentAssistanceTerminatedId } from '@module/customer/analysis-tool/module/accident-assistance-terminated/domain/schema/entity/accident-assistance-terminated/value-object/accident-assistance-terminated-id/accident-assistance-terminated-id.value-object';
+import type { AccidentAssistanceTerminatedPeriodEntity } from '@module/customer/analysis-tool/module/accident-assistance-terminated/domain/schema/entity/accident-assistance-terminated-period/accident-assistance-terminated-period.entity';
 import type { AccidentAssistanceTerminatedPeriodId } from '@module/customer/analysis-tool/module/accident-assistance-terminated/domain/schema/entity/accident-assistance-terminated-period/value-object/accident-assistance-terminated-period-id/accident-assistance-terminated-period-id.value-object';
 
 export abstract class AccidentAssistanceTerminatedPeriodCommandRepositoryGateway {
--- src/module/customer/analysis-tool/module/accident-assistance-terminated/domain/schema/entity/accident-assistance-terminated-period/accident-assistance-terminated-period.entity.props.interface.ts
+++ src/module/customer/analysis-tool/module/accident-assistance-terminated/domain/schema/entity/accident-assistance-terminated-period/accident-assistance-terminated-period.entity.props.interface.ts
@@ -3,8 +3,7 @@ import type { DecimalValue } from '@core/domain/schema/value-object/decimal/deci
 import type { AccidentAssistanceTerminatedPeriodReasonPendencyEnum } from '@module/customer/analysis-tool/module/accident-assistance-terminated/domain/schema/entity/accident-assistance-terminated-period/enum/accident-assistance-terminated-period-reason-pendency.enum';
 import type { AccidentAssistanceTerminatedPeriodId } from '@module/customer/analysis-tool/module/accident-assistance-terminated/domain/schema/entity/accident-assistance-terminated-period/value-object/accident-assistance-terminated-period-id/accident-assistance-terminated-period-id.value-object';
 
-export interface AccidentAssistanceTerminatedPeriodEntityPropsInterface
-  extends BaseEntityPropsInterface<AccidentAssistanceTerminatedPeriodId> {
+export interface AccidentAssistanceTerminatedPeriodEntityPropsInterface extends BaseEntityPropsInterface<AccidentAssistanceTerminatedPeriodId> {
   sequencial?: number | null;
   periodName?: string | null;
   periodStart?: Date | null;
