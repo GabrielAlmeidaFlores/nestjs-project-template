bc8641128bc6390fae2d1397fcf2cf1a29cdd9da
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-05-14 10:18:02 -0300

    refactor: replace temporary-incapacity-benefit-termination domain with permanent-incapacity-benefit-terminated

--- src/module/customer/analysis-tool/module/temporary-incapacity-benefit-termination/domain/repository/temporary-incapacity-benefit-termination-work-periods-earnings-history/command/temporary-incapacity-benefit-termination-work-periods-earnings-history.command.repository.gateway.ts
+++
@@ -1,8 +0,0 @@
-import type { TransactionType } from '@core/domain/repository/base/transaction/type/transaction.type';
-import type { TemporaryIncapacityBenefitTerminationWorkPeriodsEarningsHistoryEntity } from '@module/customer/analysis-tool/module/temporary-incapacity-benefit-termination/domain/schema/entity/temporary-incapacity-benefit-termination-work-periods-earnings-history/temporary-incapacity-benefit-termination-work-periods-earnings-history.entity';
-
-export abstract class TemporaryIncapacityBenefitTerminationWorkPeriodsEarningsHistoryCommandRepositoryGateway {
-  public abstract createTemporaryIncapacityBenefitTerminationWorkPeriodsEarningsHistory(
-    props: TemporaryIncapacityBenefitTerminationWorkPeriodsEarningsHistoryEntity,
-  ): TransactionType;
-}
