eada8004c1d52c8a376e83b69e1bf186997e7eee
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-05-16 16:15:09 -0300

    fix: enum

--- src/module/customer/analysis-tool/module/special-retirement-rejection/domain/schema/entity/special-retirement-rejection-work-period/enum/special-retirement-rejection-work-period-pendency-reason.enum.ts
+++ src/module/customer/analysis-tool/module/special-retirement-rejection/domain/schema/entity/special-retirement-rejection-work-period/enum/special-retirement-rejection-work-period-pendency-reason.enum.ts
@@ -1,6 +1,5 @@
 export enum SpecialRetirementRejectionWorkPeriodPendencyReasonEnum {
-  CONTRIBUICOES_ABAIXO_DO_MINIMO = 'contribuicoes_abaixo_do_minimo',
-  PERIODO_SEM_DATA_DE_SAIDA = 'periodo_sem_data_de_saida',
-  CONTRIBUICAO_EM_ATRASO = 'contribuicao_em_atraso',
-  VINCULO_AUSENTE_NO_CNIS = 'vinculo_ausente_no_cnis',
+  LEAVE_DATE = 'LEAVE_DATE',
+  COMPETENCE_BELOW_MINIMUM = 'COMPETENCE_BELOW_MINIMUM',
+  INCONSISTENT_COMPETENCE = 'INCONSISTENT_COMPETENCE',
 }
