e2785eed3f3b1070b932386a3ce08de9f3ba04b3
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-05-18 16:26:34 -0300

    fix: standardize quotes in migration for teacher retirement planning period item fields

--- src/infra/database/implementation/typeorm/migration/20260518154500-MakeTeacherRetirementPlanningPeriodItemFieldsNullable.ts
+++ src/infra/database/implementation/typeorm/migration/20260518154500-MakeTeacherRetirementPlanningPeriodItemFieldsNullable.ts
@@ -1,25 +1,24 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class MakeTeacherRetirementPlanningPeriodItemFieldsNullable20260518154500
-  implements MigrationInterface
-{
-  public name = 'MakeTeacherRetirementPlanningPeriodItemFieldsNullable20260518154500';
+export class MakeTeacherRetirementPlanningPeriodItemFieldsNullable20260518154500 implements MigrationInterface {
+  public name =
+    'MakeTeacherRetirementPlanningPeriodItemFieldsNullable20260518154500';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      "ALTER TABLE `teacher_retirement_planning_period_item` CHANGE `end_date` `end_date` date NULL",
+      'ALTER TABLE `teacher_retirement_planning_period_item` CHANGE `end_date` `end_date` date NULL',
     );
     await queryRunner.query(
-      "ALTER TABLE `teacher_retirement_planning_period_item` CHANGE `institution_name` `institution_name` varchar(255) NULL",
+      'ALTER TABLE `teacher_retirement_planning_period_item` CHANGE `institution_name` `institution_name` varchar(255) NULL',
     );
     await queryRunner.query(
-      "ALTER TABLE `teacher_retirement_planning_period_item` CHANGE `institution_type` `institution_type` varchar(255) NULL",
+      'ALTER TABLE `teacher_retirement_planning_period_item` CHANGE `institution_type` `institution_type` varchar(255) NULL',
     );
     await queryRunner.query(
-      "ALTER TABLE `teacher_retirement_planning_period_item` CHANGE `education_level` `education_level` varchar(255) NULL",
+      'ALTER TABLE `teacher_retirement_planning_period_item` CHANGE `education_level` `education_level` varchar(255) NULL',
     );
     await queryRunner.query(
-      "ALTER TABLE `teacher_retirement_planning_period_item` CHANGE `role_performed` `role_performed` varchar(255) NULL",
+      'ALTER TABLE `teacher_retirement_planning_period_item` CHANGE `role_performed` `role_performed` varchar(255) NULL',
     );
   }
 
@@ -34,10 +33,10 @@ export class MakeTeacherRetirementPlanningPeriodItemFieldsNullable20260518154500
       "ALTER TABLE `teacher_retirement_planning_period_item` CHANGE `institution_type` `institution_type` enum('private','public') NOT NULL",
     );
     await queryRunner.query(
-      "ALTER TABLE `teacher_retirement_planning_period_item` CHANGE `institution_name` `institution_name` varchar(255) NOT NULL",
+      'ALTER TABLE `teacher_retirement_planning_period_item` CHANGE `institution_name` `institution_name` varchar(255) NOT NULL',
     );
     await queryRunner.query(
-      "ALTER TABLE `teacher_retirement_planning_period_item` CHANGE `end_date` `end_date` date NOT NULL",
+      'ALTER TABLE `teacher_retirement_planning_period_item` CHANGE `end_date` `end_date` date NOT NULL',
     );
   }
 }
