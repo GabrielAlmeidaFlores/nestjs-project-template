a905ee6043e9dde0246ed0871a0ab15117725848
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-05-21 16:10:59 -0300

    feat: add use-case and DTO to get remuneration calculation for special category retirement analysis

---
+++ src/module/customer/analysis-tool/module/special-category-retirement-analysis/use-case/get-special-category-retirement-analysis-remuneration-calculation.use-case.ts
@@ -0,0 +1,56 @@
+import { Inject, Injectable } from '@nestjs/common';
+
+import { RemunerationDataInputModel } from '@module/customer/analysis-tool/lib/remuneration-calculator/model/input/remuneration-data.input.model';
+import { RemunerationCalculatorGateway } from '@module/customer/analysis-tool/lib/remuneration-calculator/remuneration-calculator.gateway';
+import { SpecialCategoryRetirementAnalysisQueryRepositoryGateway } from '@module/customer/analysis-tool/module/special-category-retirement-analysis/domain/repository/special-category-retirement-analysis/query/special-category-retirement-analysis.query.repository.gateway';
+import { SpecialCategoryRetirementAnalysisRemunerationQueryRepositoryGateway } from '@module/customer/analysis-tool/module/special-category-retirement-analysis/domain/repository/special-category-retirement-analysis-remuneration/query/special-category-retirement-analysis-remuneration.query.repository.gateway';
+import { SpecialCategoryRetirementAnalysisId } from '@module/customer/analysis-tool/module/special-category-retirement-analysis/domain/schema/entity/special-category-retirement-analysis/value-object/special-category-retirement-analysis-id/special-category-retirement-analysis-id.value-object';
+import { GetSpecialCategoryRetirementAnalysisRemunerationCalculationResponseDto } from '@module/customer/analysis-tool/module/special-category-retirement-analysis/dto/response/get-special-category-retirement-analysis-remuneration-calculation.response.dto';
+import { SpecialCategoryRetirementAnalysisNotFoundError } from '@module/customer/analysis-tool/module/special-category-retirement-analysis/error/special-category-retirement-analysis-not-found.error';
+import { OrganizationSessionDataModel } from '@shared/api/util/decorator/property/get-organization-session-data/model/generic/organization-session-data.model';
+
+@Injectable()
+export class GetSpecialCategoryRetirementAnalysisRemunerationCalculationUseCase {
+  protected readonly _type =
+    GetSpecialCategoryRetirementAnalysisRemunerationCalculationUseCase.name;
+
+  public constructor(
+    @Inject(SpecialCategoryRetirementAnalysisQueryRepositoryGateway)
+    private readonly specialCategoryRetirementAnalysisQueryRepositoryGateway: SpecialCategoryRetirementAnalysisQueryRepositoryGateway,
+    @Inject(SpecialCategoryRetirementAnalysisRemunerationQueryRepositoryGateway)
+    private readonly remunerationQueryRepositoryGateway: SpecialCategoryRetirementAnalysisRemunerationQueryRepositoryGateway,
+    @Inject(RemunerationCalculatorGateway)
+    private readonly remunerationCalculatorGateway: RemunerationCalculatorGateway,
+  ) {}
+
+  public async execute(
+    organizationSessionData: OrganizationSessionDataModel,
+    analysisId: SpecialCategoryRetirementAnalysisId,
+  ): Promise<GetSpecialCategoryRetirementAnalysisRemunerationCalculationResponseDto> {
+    await this.specialCategoryRetirementAnalysisQueryRepositoryGateway.findOneByIdAndOrganizationIdWithRelationsOrFail(
+      analysisId,
+      organizationSessionData.organizationId,
+      SpecialCategoryRetirementAnalysisNotFoundError,
+    );
+
+    const remunerations =
+      await this.remunerationQueryRepositoryGateway.listByAnalysisId(
+        analysisId,
+      );
+
+    const calculation = this.remunerationCalculatorGateway.calculate(
+      remunerations
+        .filter((remuneration) => remuneration.remunerationGrossAmount !== null)
+        .map((remuneration) =>
+          RemunerationDataInputModel.build({
+            remunerationDate: remuneration.remunerationReferenceMonthYear,
+            remunerationAmount: remuneration.remunerationGrossAmount!,
+          }),
+        ),
+    );
+
+    return GetSpecialCategoryRetirementAnalysisRemunerationCalculationResponseDto.build(
+      { ...calculation },
+    );
+  }
+}
