00c5dc4ec62807ddb02948c54f16360fec7817a7
Gabriel Flores <gabrielalmeidaflores@hotmail.com>
2026-05-22 15:47:45 -0300

    feat: add remuneration DTOs and expose remunerations in DWH request/response DTOs

--- src/module/customer/analysis-tool/module/survivor-pension-analysis/dto/request/calculate-survivor-pension-analysis-remuneration.request.dto.ts
+++ src/module/customer/analysis-tool/module/survivor-pension-analysis/dto/request/calculate-survivor-pension-analysis-remuneration.request.dto.ts
@@ -1,23 +1,16 @@
 import { SurvivorPensionAnalysisRemunerationItemRequestDto } from '@module/customer/analysis-tool/module/survivor-pension-analysis/dto/request/survivor-pension-analysis-remuneration-item.request.dto';
 import { RequestDto } from '@shared/api/util/decorator/class/dto-specification/request-dto.decorator';
-import { RequestDtoDateProperty } from '@shared/api/util/decorator/property/dto-property/request/request-dto-date-property/request-dto-date-property.decorator';
 import { RequestDtoObjectProperty } from '@shared/api/util/decorator/property/dto-property/request/request-dto-object-property/request-dto-object-property.decorator';
 import { BaseBuildableDtoObject } from '@shared/api/util/object/base-buildable-dto.object';
 
 @RequestDto()
-export class UpdateSurvivorPensionAnalysisDeceasedWorkHistoryRequestDto extends BaseBuildableDtoObject {
-  @RequestDtoDateProperty({ required: false })
-  public startDate?: Date;
-
-  @RequestDtoDateProperty({ required: false })
-  public endDate?: Date;
-
+export class CalculateSurvivorPensionAnalysisRemunerationRequestDto extends BaseBuildableDtoObject {
   @RequestDtoObjectProperty(
     () => SurvivorPensionAnalysisRemunerationItemRequestDto,
-    { required: false, isArray: true },
+    { required: true, isArray: true },
   )
-  public remunerations?: SurvivorPensionAnalysisRemunerationItemRequestDto[];
+  public readonly remunerations: SurvivorPensionAnalysisRemunerationItemRequestDto[];
 
   protected override readonly _type =
-    UpdateSurvivorPensionAnalysisDeceasedWorkHistoryRequestDto.name;
+    CalculateSurvivorPensionAnalysisRemunerationRequestDto.name;
 }
